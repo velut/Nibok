122713b604813b37a4f54f23b087210972517626
Edoardo Scibona <velut@users.noreply.github.com>
2016-05-22 17:58:48 +0200

    Add tab layout to MainActivity

--- app/src/main/java/com/nibokapp/nibok/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/activity/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.nibokapp.nibok.ui.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
 import com.nibokapp.nibok.R
+import com.nibokapp.nibok.ui.fragment.LatestFragment
+import com.nibokapp.nibok.ui.fragment.MessagesFragment
+import com.nibokapp.nibok.ui.fragment.SavedFragment
+import com.nibokapp.nibok.ui.fragment.SellingFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        setupViewPager(viewPager)
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    fun setupViewPager(viewPager: ViewPager) {
+        val fragments = mapOf(
+                "Latest" to LatestFragment(),
+                "Saved" to SavedFragment(),
+                "Selling" to SellingFragment(),
+                "Messages" to MessagesFragment()
+        )
+        val adapter = ViewPagerAdapter(supportFragmentManager, fragments)
+        viewPager.adapter = adapter
+    }
+
+    class ViewPagerAdapter(fm: FragmentManager, val fragments: Map<String, Fragment>) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment? {
+            val fragmentsInstances = fragments.values
+            return fragmentsInstances.elementAt(position)
+        }
+
+        override fun getCount(): Int = fragments.size
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            val fragmentsNames = fragments.keys
+            return fragmentsNames.elementAt(position)
+        }
+
     }
 }
