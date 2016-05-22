076e9cca9e832c790763f43d87628e4e1fa8a6bb
Edoardo Scibona <velut@users.noreply.github.com>
2016-05-22 17:54:21 +0200

    Add Latest, Saved, Selling, Messages fragments stubs

---
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/LatestFragment.kt
@@ -0,0 +1,19 @@
+package com.nibokapp.nibok.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nibokapp.nibok.R
+
+class LatestFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.latest_fragment, container, false)
+    }
+}
