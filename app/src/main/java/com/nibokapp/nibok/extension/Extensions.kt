a0f4ad6f84ba2b34b487cfc875746087ddb8433c
Edoardo Scibona <velut@users.noreply.github.com>
2016-05-24 20:13:15 +0200

    Add loadImg extension function

--- app/src/main/java/com/nibokapp/nibok/extension/Extensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/Extensions.kt
@@ -1,10 +1,22 @@
 package com.nibokapp.nibok.extension
 
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.nibokapp.nibok.R
 
 
 fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+}
+
+fun ImageView.loadImg(imageUrl: String) {
+    if (TextUtils.isEmpty(imageUrl)) {
+        Glide.with(context).load(R.mipmap.ic_launcher).into(this)
+    } else {
+        Glide.with(context).load(imageUrl).into(this)
+    }
 }
