8c385c36f9e2281d45c2f9067361722dcd780462
Edoardo Scibona <velut@users.noreply.github.com>
2016-05-24 19:33:12 +0200

    Attach BookAdapter to LatestFragment

--- app/src/main/java/com/nibokapp/nibok/ui/fragment/LatestFragment.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/LatestFragment.kt
@@ -2,18 +2,33 @@ package com.nibokapp.nibok.ui.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nibokapp.nibok.R
+import com.nibokapp.nibok.extension.inflate
+import com.nibokapp.nibok.ui.adapter.BookAdapter
+import kotlinx.android.synthetic.main.latest_fragment.*
 
 class LatestFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return container?.inflate(R.layout.latest_fragment)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.latest_fragment, container, false)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        latestBooksList.setHasFixedSize(true)
+        latestBooksList.layoutManager = LinearLayoutManager(context)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        if (latestBooksList.adapter == null) {
+            latestBooksList.adapter = BookAdapter()
+        }
     }
 }
