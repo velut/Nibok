d07adfad790c5b3362607af4453555c7d2a67f6b
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-07 17:55:30 +0200

    Add BookDetailFragment, hosted by BookDetailActivity

--- app/src/main/java/com/nibokapp/nibok/ui/activity/BookDetailActivity.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/activity/BookDetailActivity.kt
@@ -1,22 +1,34 @@
 package com.nibokapp.nibok.ui.activity
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import com.nibokapp.nibok.R
+import com.nibokapp.nibok.ui.fragment.BookDetailFragment
 
 class BookDetailActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = BookDetailActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_detail)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar?
-        setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        // If restoring do not create overlapping fragments
+        if (savedInstanceState != null) {
+            return
+        }
+
+
+        val bookDetailFragment = BookDetailFragment()
+        // Pass eventual intent extras to the fragment
+        bookDetailFragment.arguments = intent.extras
+
+        // Add the fragment to the container
+        supportFragmentManager.beginTransaction()
+                .add(R.id.detailFragmentContainer, bookDetailFragment)
+                .commit()
+
     }
 }
