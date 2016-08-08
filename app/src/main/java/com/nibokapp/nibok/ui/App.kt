be635a10c2364e35d0b3c0056212ab2fde1e684a
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-08 18:31:03 +0200

    Add Realm config to App class

--- app/src/main/java/com/nibokapp/nibok/ui/App.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/App.kt
@@ -1,7 +1,16 @@
 package com.nibokapp.nibok.ui
 
 import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class App : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        val realmConfig = RealmConfiguration.Builder(this)
+                .deleteRealmIfMigrationNeeded() // Delete the DB instead of migrating
+                .build()
+        Realm.setDefaultConfiguration(realmConfig)
+    }
 }
