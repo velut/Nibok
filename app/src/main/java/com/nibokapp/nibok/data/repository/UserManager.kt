9207a9e53e671b7d24a1f451e7c893a7c05b4eec
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-13 20:36:22 +0200

    Add user insertion retrieval and saving

--- app/src/main/java/com/nibokapp/nibok/data/repository/UserManager.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/UserManager.kt
@@ -1,8 +1,10 @@
 package com.nibokapp.nibok.data.repository
 
 import android.util.Log
+import com.nibokapp.nibok.data.db.Insertion
 import com.nibokapp.nibok.data.db.User
 import com.nibokapp.nibok.extension.queryOneWithRealm
+import com.nibokapp.nibok.extension.toNormalList
 import com.nibokapp.nibok.extension.withRealm
 
 /**
@@ -41,5 +43,53 @@ class UserManager {
             userExists = true
         }
         return userExists
+    }
+
+    fun hasUserSavedInsertions() : Boolean {
+        var result = false
+        if (!isUserCreated()) {
+            return result
+        }
+        val user = queryOneWithRealm { it.where(User::class.java).findFirst() }
+        val savedInsertions = user?.savedInsertions
+        savedInsertions?.let {
+            result = true
+        }
+        return result
+    }
+
+    fun getUserSavedInsertions() : List<Insertion> {
+        val user = queryOneWithRealm { it.where(User::class.java).findFirst() }
+        val savedBooks = user?.savedInsertions?.toNormalList()
+        var results: List<Insertion> = emptyList()
+        savedBooks?.let {
+            results = it
+        }
+        Log.d(TAG, "Returning ${results.size} saved insertions")
+        return results
+    }
+
+    fun toggleSaveInsertion(insertionId: Long) : Boolean {
+        var saved = false
+
+        if (hasUserSavedInsertions()) {
+            withRealm {
+                val insertion = it.where(Insertion::class.java).equalTo("id", insertionId).findFirst()
+                val user = it.where(User::class.java).findFirst()
+                val savedInsertions = user?.savedInsertions!!
+
+                saved = savedInsertions.contains(insertion)
+                it.executeTransaction {
+                    if (!saved) {
+                        savedInsertions.add(0, insertion)
+                    } else {
+                        savedInsertions.remove(insertion)
+                    }
+                }
+                saved = savedInsertions.contains(insertion)
+                Log.d(TAG, "Save status: $saved, saved size is ${savedInsertions.size}")
+            }
+        }
+        return saved
     }
 }
