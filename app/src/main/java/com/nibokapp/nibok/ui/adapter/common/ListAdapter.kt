b9a7db3f4d0583079db2bc6db05f78bf0410c40f
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-15 17:25:02 +0200

    Add updateItems

--- app/src/main/java/com/nibokapp/nibok/ui/adapter/common/ListAdapter.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/adapter/common/ListAdapter.kt
@@ -1,9 +1,29 @@
 package com.nibokapp.nibok.ui.adapter.common
 
+/**
+ * Interface of adapters dealing with lists of objects.
+ */
 interface ListAdapter<in T> {
 
+    /**
+     * Add items to a list.
+     *
+     * @param items the list of items to add
+     */
     fun addItems(items: List<T>)
 
+    /**
+     * Clear the list and add the given items.
+     *
+     * @param items the list of items to add
+     */
     fun clearAndAddItems(items: List<T>)
+
+    /**
+     * Update the list with the given items.
+     *
+     * @param items the new or updated items to put in the list
+     */
+    fun updateItems(items: List<T>)
 
 }
