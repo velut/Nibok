667d062dc0e05125ab307e769bd398fd62ff937a
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-16 20:54:43 +0200

    Add removal and reinsertion of book insertions in SavedFragment

--- app/src/main/java/com/nibokapp/nibok/ui/fragment/SavedFragment.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/SavedFragment.kt
@@ -1,10 +1,13 @@
 package com.nibokapp.nibok.ui.fragment
 
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import com.nibokapp.nibok.R
 import com.nibokapp.nibok.data.repository.BookManager
+import com.nibokapp.nibok.data.repository.UserManager
+import com.nibokapp.nibok.domain.model.BookModel
 import com.nibokapp.nibok.ui.adapter.BookAdapter
 import com.nibokapp.nibok.ui.adapter.common.ViewType
 import com.nibokapp.nibok.ui.fragment.common.ViewTypeFragment
@@ -26,7 +29,7 @@ class SavedFragment : ViewTypeFragment() {
 
     override fun getMainViewLayoutManager() = LinearLayoutManager(context)
 
-    override fun getMainViewAdapter() = BookAdapter()
+    override fun getMainViewAdapter() = BookAdapter { mainViewItemClickListener(it) }
 
     override fun getMainViewData(): List<ViewType> = BookManager.getSavedBooksList()
 
@@ -36,7 +39,7 @@ class SavedFragment : ViewTypeFragment() {
 
     override fun getSearchViewLayoutManager() = LinearLayoutManager(context)
 
-    override fun getSearchViewAdapter() = BookAdapter()
+    override fun getSearchViewAdapter() = BookAdapter { searchViewItemClickListener(it) }
 
     override fun searchStrategy(query: String): List<ViewType> = BookManager.getBooksFromQuery(query)
 
@@ -67,11 +70,75 @@ class SavedFragment : ViewTypeFragment() {
         if (BookManager.hasOlderSavedBooks()) {
             Log.i(TAG, "Requesting older saved books on scroll down")
             val olderBooks = BookManager.getOlderSavedBooks()
-            bookAdapter.addBooks(olderBooks, addToTop = false)
+            bookAdapter.addBooks(olderBooks, addToBottom = true)
         } else {
             Log.i(TAG, "No more older saved books, end reached")
             bookAdapter.removeLoadingItem()
             context.toast(getString(R.string.end_reached))
+        }
+    }
+
+    /**
+     * Remove book insertions once unsaved, notify the user and offer the possibility to restore the
+     * insertion after removal.
+     *
+     * @param item the item that was clicked
+     */
+    private fun mainViewItemClickListener(item: ViewType) {
+        bookItemClickListener(item) {
+            val book = it
+            if (!book.saved) { // If book was removed
+                val mainViewAdapter = getMainView().adapter as? BookAdapter
+                mainViewAdapter?.let {
+
+                    // Save old book position for possible reinsertion
+                    val oldBookPosition = mainViewAdapter.removeBook(book)
+
+                    // Notify user of removal
+                    val snackBar = Snackbar.make(relativeLayoutContainer,
+                            R.string.book_removed_from_collection, Snackbar.LENGTH_LONG)
+
+                    // Provide reinsertion possibility
+                    snackBar.setAction(R.string.snackbar_undo_action) {
+                        // Reinsert book
+                        book.saved = UserManager.toggleSaveInsertion(book.insertionId)
+                        mainViewAdapter.addBooks(listOf(book), insertAtPosition = oldBookPosition)
+
+                        // Notify the reinsertion
+                        val childSnackBar = Snackbar.make(relativeLayoutContainer,
+                                R.string.book_reinserted_into_collection, Snackbar.LENGTH_SHORT)
+                        childSnackBar.show()
+                    }
+                    snackBar.show()
+                }
+            }
+        }
+    }
+
+    /**
+     * Notify with toasts the user of the saved status of the clicked book insertion.
+     *
+     * @param item the item that was clicked
+     */
+    private fun searchViewItemClickListener(item: ViewType) {
+        bookItemClickListener(item) {
+            val toastMessage = if (it.saved) R.string.book_saved_to_collection
+            else R.string.book_removed_from_collection
+            context.toast(toastMessage)
+        }
+    }
+
+    /**
+     * Handle click on books.
+     *
+     * @param item the item that was clicked
+     * @param onClick the function to execute given the clicked item
+     */
+    private fun bookItemClickListener(item: ViewType, onClick: (BookModel) -> Unit) {
+        val book = item as? BookModel
+        book?.let {
+            Log.d(TAG, "Handling item click")
+            onClick(it)
         }
     }
 }
