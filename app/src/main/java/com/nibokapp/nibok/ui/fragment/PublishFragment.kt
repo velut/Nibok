bda1d72004b5ba8931c5e96ce01141bdcba241e6
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-18 20:23:23 +0200

    Add isbn input field and validation in PublishFragment

--- app/src/main/java/com/nibokapp/nibok/ui/fragment/PublishFragment.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/PublishFragment.kt
@@ -3,6 +3,9 @@ package com.nibokapp.nibok.ui.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +17,9 @@ class PublishFragment : Fragment() {
 
     companion object {
         private val TAG = PublishFragment::class.java.simpleName
+        val ISBN_13_LENGTH = 13
+        val ISBN_PREFIXES = listOf("977", "978", "979")
+        val ISBN_PREFIX_LENGTH = 3
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -23,10 +29,53 @@ class PublishFragment : Fragment() {
         val hostingActivity = (activity as AppCompatActivity)
         hostingActivity.setSupportActionBar(toolbar)
         hostingActivity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return container?.inflate(R.layout.fragment_publish)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        inputISBN.addTextChangedListener(
+                object : TextWatcher {
+                    override fun afterTextChanged(s: Editable?) {
+                        validateISBNInput()
+                    }
+
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                }
+        )
+    }
+
+    private fun validateISBNInput() {
+        val isbn = inputISBN.text.toString().trim()
+        when (isbn.length) {
+            ISBN_13_LENGTH -> parseISBN(isbn)
+            else -> {
+                inputISBNLayout.error = getString(R.string.error_input_isbn)
+                inputISBNLayout.requestFocus()}
+        }
+    }
+
+    private fun parseISBN(isbn: String) {
+        val isbnPrefix = isbn.substring(0, ISBN_PREFIX_LENGTH)
+        when (isbnPrefix) {
+            in ISBN_PREFIXES -> {
+                inputISBNLayout.error = null
+                getBookDataFromISBN(isbn)
+            }
+            else -> {
+                inputISBNLayout.error = getString(R.string.error_invalid_isbn)
+                inputISBNLayout.requestFocus()
+            }
+        }
+    }
+
+    private fun getBookDataFromISBN(isbn: String) {
+        Log.d(TAG, "Valid Isbn: $isbn")
     }
 }
