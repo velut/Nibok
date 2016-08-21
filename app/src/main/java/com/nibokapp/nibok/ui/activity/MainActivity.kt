fa885edf8d8b78d6589ba3d1410e7879058e9215
Edoardo Scibona <velut@users.noreply.github.com>
2016-08-21 18:48:36 +0200

    Update viewpager adapter to check for existing fragment instances to use after rotation

--- app/src/main/java/com/nibokapp/nibok/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/activity/MainActivity.kt
@@ -12,14 +12,17 @@ import com.nibokapp.nibok.ui.fragment.LatestFragment
 import com.nibokapp.nibok.ui.fragment.MessagesFragment
 import com.nibokapp.nibok.ui.fragment.SavedFragment
 import com.nibokapp.nibok.ui.fragment.SellingFragment
-import com.nibokapp.nibok.ui.fragment.common.BaseFragment
 import com.nibokapp.nibok.ui.fragment.common.VisibleFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * The main activity of the application.
  *
- * It sets up the ViewPager with the four main fragments.
+ * It hosts the ViewPager with the four main fragments:
+ *  LatestFragment: the fragment for the feed of latest insertions published
+ *  SavedFragment: the fragment for the list of insertions bookmarked by the user
+ *  SellingFragment: the fragment for the list of insertions published by the user
+ *  MessagesFragment: the fragment for the list of messages exchanged with other users
  */
 class MainActivity : AppCompatActivity() {
 
@@ -30,31 +33,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(toolbar)
-        setupViewPager(viewPager)
-        tabLayout.setupWithViewPager(viewPager)
-    }
 
-    /**
-     * Sets up the ViewPager adding the four main fragments and the adapter.
-     *
-     * @param viewPager the ViewPager defined in the layout
-     */
-    fun setupViewPager(viewPager: ViewPager) {
-        val fragments = mapOf(
+        val fragments = mapOf<String, Fragment>(
                 getString(R.string.latest_tab) to LatestFragment(),
                 getString(R.string.saved_tab) to SavedFragment(),
                 getString(R.string.selling_tab) to SellingFragment(),
                 getString(R.string.messages_tab) to MessagesFragment()
         )
+
+        setupViewPager(viewPager, fragments)
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+
+
+    /**
+     * Sets up the ViewPager linking it to the adapter and adding the given fragments.
+     *
+     * @param viewPager the ViewPager defined in the layout
+     * @param fragments the fragments representing the pages to display in the viewpager
+     */
+    fun setupViewPager(viewPager: ViewPager, fragments: Map<String, Fragment>) {
+
         val adapter = ViewPagerAdapter(supportFragmentManager, fragments)
         viewPager.adapter = adapter
         viewPager.addOnPageChangeListener(
                 object : ViewPager.OnPageChangeListener {
                     override fun onPageSelected(position: Int) {
                         val fragment = adapter.getItem(position) as? VisibleFragment
-                        Log.d("$TAG ViewPager", "Fragment at position $position became visible")
+                        Log.d("$TAG ViewPager", "Fragment $fragment at position $position became visible")
                         fragment?.onBecomeVisible()
 
                         // Alert other fragments that they became invisible
@@ -71,11 +79,15 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    class ViewPagerAdapter(fm: FragmentManager, val fragments: Map<String, BaseFragment>) : FragmentPagerAdapter(fm) {
+    class ViewPagerAdapter(val fm: FragmentManager, val fragments: Map<String, Fragment>) : FragmentPagerAdapter(fm) {
 
         override fun getItem(position: Int): Fragment? {
-            val fragmentsInstances = fragments.values
-            return fragmentsInstances.elementAt(position)
+            // If the fragment manager already has a fragment with the given tag
+            // then return the stored fragment instead of a new instance
+            // This solves the problems of duplicate fragments and blank views after rotation
+            // caused by the viewpager
+            val foundFragment = fm.findFragmentByTag(getFragmentTagForPosition(position))
+            return foundFragment ?: fragments.values.elementAt(position)
         }
 
         override fun getCount(): Int = fragments.size
@@ -85,5 +97,11 @@ class MainActivity : AppCompatActivity() {
             return fragmentsNames.elementAt(position)
         }
 
+        /**
+         * Return the tag given by the adapter to the fragment.
+         *
+         * @return the string representing the tag of the fragment at the given position
+         */
+        fun getFragmentTagForPosition(position: Int) = "android:switcher:${R.id.viewPager}:$position"
     }
 }
