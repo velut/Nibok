57c168ea2c486f70d658efe4d060d7aa3c008c42
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-02 18:20:09 +0200

    Add getTypeName() to AdapterTypes

--- app/src/main/java/com/nibokapp/nibok/ui/adapter/common/AdapterTypes.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/adapter/common/AdapterTypes.kt
@@ -1,10 +1,25 @@
 package com.nibokapp.nibok.ui.adapter.common
 
 /**
- * The types of adapters to be associated to adapter instances.
+ * Enumeration of adapter types for the delegate adapters.
  */
 object AdapterTypes {
     val LOADING = 1
     val BOOK = 2
     val MESSAGE = 3
+
+    private val typeNames = mapOf(
+            LOADING to "Loading",
+            BOOK to "Book",
+            MESSAGE to "Message"
+    )
+
+    /**
+     * Get the name associated to a given type.
+     *
+     * @param type the type of which the name is needed
+     *
+     * @return the name of the type or "Wrong type" if no such type is present.
+     */
+    fun getTypeName(type: Int) = typeNames[type] ?: "Wrong type"
 }
