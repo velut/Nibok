cd042ffe99ee17c9b4ca71ec5ea8bf7ae20edbf9
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-06 19:49:33 +0200

    Add chat messages view setup and user id retrieval

--- app/src/main/java/com/nibokapp/nibok/ui/fragment/ChatFragment.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/ChatFragment.kt
@@ -3,22 +3,46 @@ package com.nibokapp.nibok.ui.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nibokapp.nibok.R
+import com.nibokapp.nibok.data.repository.UserManager
+import com.nibokapp.nibok.domain.model.ChatMessage
 import com.nibokapp.nibok.extension.inflate
+import com.nibokapp.nibok.ui.adapter.ChatAdapter
 import kotlinx.android.synthetic.main.fragment_chat.*
+import java.util.*
 
 class ChatFragment() : Fragment() {
 
     companion object {
         private val TAG = ChatFragment::class.java.simpleName
-        val CONVERSATION_ID = "$TAG:insertionId"
+        val CONVERSATION_ID = "$TAG:conversationId"
     }
+
+    val chatAdapter = ChatAdapter(getUserId())
+    val chatLayoutManager = LinearLayoutManager(context)
+
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return container?.inflate(R.layout.fragment_chat)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO remove test messages
+        chatAdapter.addMessages((1..10).map {
+            val date = Calendar.getInstance()
+            date.add(Calendar.DATE, -10 + it)
+            val content = arrayOf("Lorem Ipsum", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem Ipsum")
+            val user = it % 2
+            ChatMessage(1L, user.toLong(), content[user], date.time)
+        })
+
+        setupChatMessagesView()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,6 +52,24 @@ class ChatFragment() : Fragment() {
         val hostingActivity = (activity as AppCompatActivity)
         hostingActivity.setSupportActionBar(toolbar)
         hostingActivity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    /**
+     * Get the id of the local user.
+     *
+     * @return the id of the local user
+     */
+    private fun getUserId(): Long = UserManager.getUserId()
+
+    private fun setupChatMessagesView() {
+        Log.d(TAG, "Setting up chat messages view")
+        chatMessagesView.apply {
+            setHasFixedSize(true)
+            // Show last element (auto scroll to bottom)
+            chatLayoutManager.stackFromEnd = true
+            layoutManager = chatLayoutManager
+            adapter = chatAdapter
+        }
     }
 }
 
