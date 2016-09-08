6b46ac51eee2eef0d7044189958ac144307b627d
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-08 20:51:18 +0200

    Add presenter to ChatFragment. Add timer to check for new messages. Update sendMessage()

--- app/src/main/java/com/nibokapp/nibok/ui/fragment/ChatFragment.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/ChatFragment.kt
@@ -9,22 +9,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nibokapp.nibok.R
-import com.nibokapp.nibok.data.repository.UserManager
 import com.nibokapp.nibok.domain.model.ChatMessageModel
 import com.nibokapp.nibok.extension.inflate
 import com.nibokapp.nibok.ui.adapter.ChatAdapter
+import com.nibokapp.nibok.ui.presenter.ChatPresenter
 import kotlinx.android.synthetic.main.fragment_chat.*
+import org.jetbrains.anko.toast
 import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
-class ChatFragment() : Fragment() {
+class ChatFragment(val presenter: ChatPresenter = ChatPresenter()) : Fragment() {
 
     companion object {
         private val TAG = ChatFragment::class.java.simpleName
         val CONVERSATION_ID = "$TAG:conversationId"
+
+        /**
+         * Timer constants.
+         * Delay before start is 2 seconds, execution period is 10 seconds
+         */
+        const private val TIMER_DELAY =  (2 * 1000).toLong()
+        const private val TIMER_PERIOD = (10 * 1000).toLong()
     }
 
-    val chatAdapter = ChatAdapter(getUserId())
-    val chatLayoutManager = LinearLayoutManager(context)
+    private val chatAdapter = ChatAdapter(getUserId())
+    private val chatLayoutManager = LinearLayoutManager(context)
+
+    lateinit var checkNewMessagesTimer : Timer
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -33,14 +44,10 @@ class ChatFragment() : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO remove test messages
-        chatAdapter.addMessages((1..10).map {
-            val date = Calendar.getInstance()
-            date.add(Calendar.DATE, -10 + it)
-            val content = arrayOf("Lorem Ipsum", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem Ipsum")
-            val user = it % 2
-            ChatMessageModel(1L, user.toLong(), content[user], date.time)
-        })
+
+        // TODO use real conversation id
+        val chatMessages = presenter.getConversationMessages(0)
+        chatAdapter.addMessages(chatMessages)
 
         setupChatMessagesView()
         addSendButtonLister()
@@ -55,16 +62,68 @@ class ChatFragment() : Fragment() {
         hostingActivity.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Timer: Starting to check for new messages")
+        checkNewMessagesTimer = fixedRateTimer(initialDelay = TIMER_DELAY, period = TIMER_PERIOD) {
+            Log.d(TAG, "Timer: Checking for new messages")
+            val lastMessage = chatAdapter.getLastMessage()
+            val newMessages = presenter.getNewMessages(0, lastMessage)
+            activity.runOnUiThread {
+                addNewMessages(newMessages)
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Timer: Stopping, no more check for new messages ")
+        checkNewMessagesTimer.cancel()
+    }
+
+    private fun addNewMessages(newMessages: List<ChatMessageModel>) {
+
+        if (newMessages.isEmpty()) return
+
+        val lm = chatMessagesView.layoutManager as LinearLayoutManager
+        val viewIsAtBottom = lm.findLastVisibleItemPosition() == (lm.itemCount - 1)
+        val newBottomPosition = chatAdapter.addMessages(newMessages)
+
+        // After adding the new messages if the view was at the bottom scroll to the newer bottom
+        // to make the new messages visible, instead if the user is reading older messages simply
+        // alert him that the partner has responded
+        if (viewIsAtBottom) {
+            chatMessagesView.smoothScrollToPosition(newBottomPosition)
+        } else {
+            // TODO Use real partner name
+            val toastMessage =
+                    String.format(resources.getString(R.string.partner_reply), "PARTNER")
+            context.toast(toastMessage)
+        }
+    }
+
     private fun addSendButtonLister() {
         btnSendMessage.setOnClickListener {
-            val messageText = chatInputText.text.trim().toString()
-            chatInputText.text.clear()
-            if (messageText.isNotEmpty()) {
-                // TODO retrieve real ids and send message
-                val message = ChatMessageModel(0, getUserId(), messageText, Calendar.getInstance().time)
+            sendMessage()
+        }
+    }
+
+    private fun sendMessage() {
+        val messageText = chatInputText.text.trim().toString()
+        if (messageText.isNotEmpty()) {
+            // TODO retrieve real ids and send message
+            val message = ChatMessageModel(0, getUserId(), messageText, Calendar.getInstance().time)
+            val messageSent = presenter.sendMessage(message)
+            if (messageSent) {
+                chatInputText.text.clear()
                 val messagePosition = chatAdapter.addMessage(message)
                 chatMessagesView.smoothScrollToPosition(messagePosition)
+            } else {
+                context.toast(R.string.error_message_not_sent)
             }
+        } else {
+            // Clear input if it contained only whitespace
+            chatInputText.text.clear()
         }
     }
 
@@ -73,7 +132,7 @@ class ChatFragment() : Fragment() {
      *
      * @return the id of the local user
      */
-    private fun getUserId(): Long = UserManager.getUserId()
+    private fun getUserId(): Long = presenter.getUserId()
 
     private fun setupChatMessagesView() {
         Log.d(TAG, "Setting up chat messages view")
