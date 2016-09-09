7b819a8f89964577b78e1ab737c68d9b2b9c9f63
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-09 18:19:07 +0200

    Add PublishInsertionPresenter stub

---
+++ app/src/main/java/com/nibokapp/nibok/ui/presenter/PublishInsertionPresenter.kt
@@ -0,0 +1,30 @@
+package com.nibokapp.nibok.ui.presenter
+
+/**
+ * Presenter for the insertion's publishing view.
+ */
+class PublishInsertionPresenter {
+
+    /**
+     * Get book data given a book ISBN code.
+     *
+     * @param isbnCode the isbn code of a book
+     *
+     * @return data about the book with the given isbn code or null if not data was found
+     */
+    fun getBookDataFromISBN(isbnCode: String) {
+        // TODO
+    }
+
+    /**
+     * Publish the given insertion.
+     *
+     * @param insertion the insertion to publish TODO
+     *
+     * @return true if the insertion was successfully published, false otherwise
+     */
+    fun publishInsertion() : Boolean {
+        // TODO
+        return false
+    }
+}
