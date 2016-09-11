bac93ba9469ef21f9baceb64521fd0245992f34f
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-11 21:00:09 +0200

    Implement RequestLocalUserIdCommand

--- app/src/main/java/com/nibokapp/nibok/domain/command/user/RequestLocalUserIdCommand.kt
+++ app/src/main/java/com/nibokapp/nibok/domain/command/user/RequestLocalUserIdCommand.kt
@@ -1,12 +1,17 @@
 package com.nibokapp.nibok.domain.command.user
 
+import com.nibokapp.nibok.data.repository.UserRepository
+import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
 import com.nibokapp.nibok.domain.command.common.Command
 
 /**
  * Request the id of the local user.
  *
  */
-class RequestLocalUserIdCommand : Command<Long> {
+class RequestLocalUserIdCommand(
+        val userRepository: UserRepositoryInterface = UserRepository
+) : Command<Long> {
 
-    override fun execute(): Long = 0L
+    override fun execute(): Long =
+            userRepository.getLocalUserId()
 }
