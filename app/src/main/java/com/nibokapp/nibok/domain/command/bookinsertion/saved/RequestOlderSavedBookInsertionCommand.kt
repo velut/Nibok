7c75832a20236d90e0b9c105793c81633853dda1
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-11 20:37:24 +0200

    Implement Saved requests

--- app/src/main/java/com/nibokapp/nibok/domain/command/bookinsertion/saved/RequestOlderSavedBookInsertionCommand.kt
+++ app/src/main/java/com/nibokapp/nibok/domain/command/bookinsertion/saved/RequestOlderSavedBookInsertionCommand.kt
@@ -1,6 +1,10 @@
 package com.nibokapp.nibok.domain.command.bookinsertion.saved
 
+import com.nibokapp.nibok.data.repository.BookInsertionRepository
+import com.nibokapp.nibok.data.repository.common.BookInsertionRepositoryInterface
 import com.nibokapp.nibok.domain.command.common.Command
+import com.nibokapp.nibok.domain.mapper.BookInsertionDataMapper
+import com.nibokapp.nibok.domain.mapper.BookInsertionDataMapperInterface
 import com.nibokapp.nibok.domain.model.BookInsertionModel
 
 /**
@@ -9,8 +13,17 @@ import com.nibokapp.nibok.domain.model.BookInsertionModel
  *
  * @param lastBookInsertion the last book insertion before the older ones
  */
-class RequestOlderSavedBookInsertionCommand(val lastBookInsertion: BookInsertionModel) :
+class RequestOlderSavedBookInsertionCommand(
+        val lastBookInsertion: BookInsertionModel,
+        val dataMapper: BookInsertionDataMapperInterface = BookInsertionDataMapper(),
+        val bookRepository: BookInsertionRepositoryInterface = BookInsertionRepository
+) :
         Command<List<BookInsertionModel>> {
 
-    override fun execute(): List<BookInsertionModel> = emptyList()
+    override fun execute(): List<BookInsertionModel> =
+            dataMapper.convertInsertionListToDomain(
+                    bookRepository.getSavedBookInsertionListBeforeDate(
+                            lastBookInsertion.insertionDate
+                    )
+            )
 }
