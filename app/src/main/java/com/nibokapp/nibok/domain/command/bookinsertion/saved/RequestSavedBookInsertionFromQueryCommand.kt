fc25879d062acbcb2a69384b6f5988799ab3af02
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-12 18:36:58 +0200

    Add book insertions query request commands

---
+++ app/src/main/java/com/nibokapp/nibok/domain/command/bookinsertion/saved/RequestSavedBookInsertionFromQueryCommand.kt
@@ -0,0 +1,24 @@
+package com.nibokapp.nibok.domain.command.bookinsertion.saved
+
+import com.nibokapp.nibok.data.repository.BookInsertionRepository
+import com.nibokapp.nibok.data.repository.common.BookInsertionRepositoryInterface
+import com.nibokapp.nibok.domain.command.common.Command
+import com.nibokapp.nibok.domain.mapper.BookInsertionDataMapper
+import com.nibokapp.nibok.domain.mapper.BookInsertionDataMapperInterface
+import com.nibokapp.nibok.domain.model.BookInsertionModel
+
+/**
+ * Request the latest BookInsertionModel instances that make up the user saved books insertions
+ * matching the given query.
+ */
+class RequestSavedBookInsertionFromQueryCommand(
+        val query: String,
+        val dataMapper: BookInsertionDataMapperInterface = BookInsertionDataMapper(),
+        val bookRepository: BookInsertionRepositoryInterface = BookInsertionRepository
+) : Command<List<BookInsertionModel>> {
+
+    override fun execute(): List<BookInsertionModel> =
+            dataMapper.convertInsertionListToDomain(
+                    bookRepository.getSavedBookInsertionListFromQuery(query)
+            )
+}
