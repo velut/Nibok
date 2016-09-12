efafcaf6636f6adcd13ea52dc885738db66abc8d
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-12 18:57:13 +0200

    Implement UserInsertionPresenter

--- app/src/main/java/com/nibokapp/nibok/ui/presenter/viewtype/UserInsertionPresenter.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/presenter/viewtype/UserInsertionPresenter.kt
@@ -1,5 +1,8 @@
 package com.nibokapp.nibok.ui.presenter.viewtype
 
+import android.util.Log
+import com.nibokapp.nibok.domain.command.bookinsertion.published.*
+import com.nibokapp.nibok.domain.model.BookInsertionModel
 import com.nibokapp.nibok.ui.adapter.viewtype.common.ViewType
 import com.nibokapp.nibok.ui.presenter.viewtype.common.ViewTypePresenter
 
@@ -8,15 +11,32 @@ import com.nibokapp.nibok.ui.presenter.viewtype.common.ViewTypePresenter
  */
 class UserInsertionPresenter: ViewTypePresenter {
 
-    private var cachedData: List<ViewType> = emptyList()
-
-    override fun getData(): List<ViewType> {
-        return emptyList()
+    companion object {
+        private val TAG = UserInsertionPresenter::class.java.simpleName
     }
 
-    override fun getCachedData(): List<ViewType> = cachedData
+    override fun getData(): List<ViewType> {
+        Log.d(TAG, "Getting published data")
+        return RequestPublishedBookInsertionCommand().execute()
+    }
+
+    override fun getCachedData(): List<ViewType> {
+        Log.d(TAG, "Getting cached published data")
+        return RequestCachedPublishedBookInsertionCommand().execute()
+    }
+
+    override fun getDataNewerThanItem(item: ViewType): List<ViewType> {
+        Log.d(TAG, "Getting newer published data")
+        return RequestNewerPublishedBookInsertionCommand(item as BookInsertionModel).execute()
+    }
+
+    override fun getDataOlderThanItem(item: ViewType): List<ViewType> {
+        Log.d(TAG, "Getting older published data")
+        return RequestOlderPublishedBookInsertionCommand(item as BookInsertionModel).execute()
+    }
 
     override fun getQueryData(query: String): List<ViewType> {
-        return emptyList()
+        Log.d(TAG, "Getting published query data")
+        return RequestPublishedBookInsertionFromQueryCommand(query).execute()
     }
 }
