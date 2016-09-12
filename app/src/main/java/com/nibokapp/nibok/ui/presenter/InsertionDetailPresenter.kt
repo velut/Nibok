53459363b1bf62aa71bf2156c47032cd26da6968
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-12 20:03:12 +0200

    Implement getInsertionDetails() in InsertionDetailPresenter

--- app/src/main/java/com/nibokapp/nibok/ui/presenter/InsertionDetailPresenter.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/presenter/InsertionDetailPresenter.kt
@@ -1,12 +1,17 @@
 package com.nibokapp.nibok.ui.presenter
 
-import com.nibokapp.nibok.data.repository.BookManager
+import android.util.Log
+import com.nibokapp.nibok.domain.command.bookinsertion.RequestBookInsertionByIdCommand
 import com.nibokapp.nibok.domain.model.BookInsertionModel
 
 /**
  * Presenter that retrieves full details about an insertion.
  */
 class InsertionDetailPresenter {
+
+    companion object {
+        private val TAG = InsertionDetailPresenter::class.java.simpleName
+    }
 
     /**
      * Get detailed data about the insertion with the given id.
@@ -15,6 +20,8 @@ class InsertionDetailPresenter {
      *
      * @return detail data about the insertion if the insertion was found, null otherwise
      */
-    fun getInsertionDetails(insertionId: Long) : BookInsertionModel? =
-            BookManager.getInsertionDetails(insertionId)
+    fun getInsertionDetails(insertionId: Long) : BookInsertionModel? {
+        Log.d(TAG, "Getting book insertion $insertionId details")
+        return RequestBookInsertionByIdCommand(insertionId).execute()
+    }
 }
