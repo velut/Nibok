eef1bc6402ebde558355ca172b697be66d159e35
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-12 18:37:39 +0200

    Use commands in InsertionFeedPresenter

--- app/src/main/java/com/nibokapp/nibok/ui/presenter/viewtype/InsertionFeedPresenter.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/presenter/viewtype/InsertionFeedPresenter.kt
@@ -1,8 +1,9 @@
 package com.nibokapp.nibok.ui.presenter.viewtype
 
-import com.nibokapp.nibok.data.repository.BookInsertionRepository
-import com.nibokapp.nibok.data.repository.BookManager
-import com.nibokapp.nibok.data.repository.common.BookInsertionRepositoryInterface
+import android.util.Log
+import com.nibokapp.nibok.domain.command.bookinsertion.feed.RequestBookInsertionFeedCommand
+import com.nibokapp.nibok.domain.command.bookinsertion.feed.RequestBookInsertionFeedFromQueryCommand
+import com.nibokapp.nibok.domain.command.bookinsertion.feed.RequestCachedBookInsertionFeedCommand
 import com.nibokapp.nibok.ui.adapter.viewtype.common.ViewType
 import com.nibokapp.nibok.ui.presenter.viewtype.common.InsertionSaveStatusPresenter
 import com.nibokapp.nibok.ui.presenter.viewtype.common.ViewTypePresenter
@@ -11,19 +12,25 @@ import com.nibokapp.nibok.ui.presenter.viewtype.common.ViewTypePresenter
  * Presenter that operates on the feed of books' insertions.
  */
 class InsertionFeedPresenter(
-        override val insertionRepository: BookInsertionRepositoryInterface = BookInsertionRepository
+        override val TAG: String = InsertionFeedPresenter.TAG
 ) : ViewTypePresenter, InsertionSaveStatusPresenter {
 
-    private var cachedData: List<ViewType> = emptyList()
-
-    override fun getData(): List<ViewType> {
-        cachedData = BookManager.getFeedBooksList()
-        return cachedData
+    companion object {
+        private val TAG : String = InsertionSaveStatusPresenter::class.java.simpleName
     }
 
-    override fun getCachedData(): List<ViewType> = cachedData
+    override fun getData(): List<ViewType> {
+        Log.d(TAG, "Getting feed data")
+        return RequestBookInsertionFeedCommand().execute()
+    }
+
+    override fun getCachedData(): List<ViewType> {
+        Log.d(TAG, "Getting cached feed data")
+        return RequestCachedBookInsertionFeedCommand().execute()
+    }
 
     override fun getQueryData(query: String): List<ViewType> {
-        return BookManager.getBooksFromQuery(query)
+        Log.d(TAG, "Getting feed query data")
+        return RequestBookInsertionFeedFromQueryCommand(query).execute()
     }
 }
