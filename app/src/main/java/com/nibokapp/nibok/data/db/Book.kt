cde4227542dd04bceb5907db5d2bd8f163146a4c
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-12 17:43:27 +0200

    Remove id field from db class Book

--- app/src/main/java/com/nibokapp/nibok/data/db/Book.kt
+++ app/src/main/java/com/nibokapp/nibok/data/db/Book.kt
@@ -3,21 +3,18 @@ package com.nibokapp.nibok.data.db
 import com.nibokapp.nibok.data.db.common.RealmString
 import io.realm.RealmList
 import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
 
 /**
  * Model class for the books.
  * Used by the local database.
  *
- * @param id the id of the book - Primary Key
+ * @param title the title of the book
  * @param authors the list of authors of the book
  * @param year the year in which the book was published
  * @param publisher the publisher of the book
  * @param isbn the isbn of the book
  */
 open class Book(
-
-        @PrimaryKey open var id: Long = 0,
 
         open var title: String = "",
 
