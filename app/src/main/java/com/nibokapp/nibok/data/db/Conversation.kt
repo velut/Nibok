27b1f5b9f4928e9db53e88e0271518e12f901e18
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-14 18:23:05 +0200

    Add userId and date fields to Conversation schema

--- app/src/main/java/com/nibokapp/nibok/data/db/Conversation.kt
+++ app/src/main/java/com/nibokapp/nibok/data/db/Conversation.kt
@@ -3,20 +3,27 @@ package com.nibokapp.nibok.data.db
 import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import java.util.*
 
 /**
  * Model class for a conversation between the local user and an external user (partner).
  * Used by the local database.
  *
  * @param id the id of the conversation
+ * @param userId the id of the local user
  * @param partner the external user participating in the conversation
+ * @param date the date in which the conversation was started
  * @param messages the list of messages exchanged in the conversation - One to many
  */
 open class Conversation(
 
         @PrimaryKey open var id: Long = 0,
 
+        open var userId: Long = 0,
+
         open var partner: ExternalUser? = null,
+
+        open var date: Date? = null,
 
         open var messages: RealmList<Message> = RealmList()
 
