68f77cdf22b12e60219f37f274ed051a478519a4
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-14 18:56:13 +0200

    Add docs

--- app/src/main/java/com/nibokapp/nibok/domain/mapper/user/UserMapper.kt
+++ app/src/main/java/com/nibokapp/nibok/domain/mapper/user/UserMapper.kt
@@ -3,6 +3,9 @@ package com.nibokapp.nibok.domain.mapper.user
 import com.nibokapp.nibok.data.db.ExternalUser
 import com.nibokapp.nibok.domain.model.UserModel
 
+/**
+ * User mapper implementation.
+ */
 class UserMapper() : UserMapperInterface {
 
     override fun convertUserToDomain(user: ExternalUser): UserModel = with(user) {
