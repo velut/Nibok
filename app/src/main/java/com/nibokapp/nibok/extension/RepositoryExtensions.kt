6b78e652a403c213829f7635120f536ef362ccd7
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-15 18:36:34 +0200

    Add getExternalUserById() extension

--- app/src/main/java/com/nibokapp/nibok/extension/RepositoryExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/RepositoryExtensions.kt
@@ -1,6 +1,7 @@
 package com.nibokapp.nibok.extension
 
 import com.nibokapp.nibok.data.db.Conversation
+import com.nibokapp.nibok.data.db.ExternalUser
 import com.nibokapp.nibok.data.db.Insertion
 import com.nibokapp.nibok.data.db.User
 import io.realm.Realm
@@ -21,6 +22,16 @@ import io.realm.Realm
 fun Realm.getLocalUser() : User? =
         this.where(User::class.java).findFirst()
 
+/**
+ * Get an external user by id.
+ *
+ * @param userId the external user's id
+ *
+ * @return the ExternalUser if it exits, null otherwise
+ */
+fun Realm.getExternalUserById(userId: Long) : ExternalUser? =
+        this.where(ExternalUser::class.java).equalTo("id", userId).findFirst()
+
 /*
  * BOOK INSERTION
  */
@@ -38,6 +49,7 @@ fun Realm.getBookInsertionById(insertionId: Long) : Insertion? =
 /*
  * CONVERSATION
  */
+
 /**
  * Get conversation by id.
  *
@@ -47,3 +59,17 @@ fun Realm.getBookInsertionById(insertionId: Long) : Insertion? =
  */
 fun Realm.getConversationById(conversationId: Long) : Conversation? =
         this.where(Conversation::class.java).equalTo("id", conversationId).findFirst()
+
+/**
+ * Get the conversation between the two given users.
+ *
+ * @param localUserId the id of the local user
+ * @param partnerId the id of the conversation's partner
+ *
+ * @return the Conversation between the two users if it exits, null otherwise
+ */
+fun Realm.getConversationBetweenUsers(localUserId: Long, partnerId: Long) : Conversation? =
+        this.where(Conversation::class.java)
+                .equalTo("userId", localUserId)
+                .equalTo("partner.id", partnerId)
+                .findFirst()
