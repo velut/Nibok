d2932ccc821f2ccc28737f7944798834ec67a874
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-17 20:53:13 +0200

    Add LoginActivity

---
+++ app/src/main/java/com/nibokapp/nibok/ui/activity/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.nibokapp.nibok.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.nibokapp.nibok.R
+
+/**
+ * Login activity.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
