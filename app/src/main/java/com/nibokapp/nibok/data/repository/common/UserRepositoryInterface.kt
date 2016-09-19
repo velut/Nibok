bf97d9cba4ff06d458d4bdd01b22ed64a1359b6e
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-19 16:14:53 +0200

    Add signUp() and login() to UserRepositoryInterface and update createUser()

--- app/src/main/java/com/nibokapp/nibok/data/repository/common/UserRepositoryInterface.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/common/UserRepositoryInterface.kt
@@ -1,21 +1,45 @@
 package com.nibokapp.nibok.data.repository.common
 
+import com.nibokapp.nibok.data.db.Conversation
+import com.nibokapp.nibok.data.db.Insertion
 import com.nibokapp.nibok.data.db.User
 
 
 interface UserRepositoryInterface {
 
     /**
+     * Sign up as a new user with the given username and password.
+     *
+     * @param username the user's unique username
+     * @param password the user's password
+     *
+     * @return true if the sign up was successful, false otherwise
+     */
+    fun signUp(username: String, password: String) : Boolean
+
+    /**
+     * Login as a returning user with the given username and password.
+     *
+     * @param username the user's unique username
+     * @param password the user's password
+     *
+     * @return true if the login was successful, false otherwise
+     */
+    fun login(username: String, password: String) : Boolean
+
+    /**
      * Create the local user instance in the local db if necessary.
      */
-    fun createLocalUser()
+    fun createLocalUser(userId: String, savedInsertions: List<Insertion> = emptyList(),
+                        publishedInsertions: List<Insertion> = emptyList(),
+                        conversations: List<Conversation> = emptyList())
 
     /**
      * Get the id of the local user if the user exists.
      *
      * @return the user's id if the user exists, an IllegalStateException if the user does not exist
      */
-    fun getLocalUserId(): Long
+    fun getLocalUserId(): String
 
     /**
      * Get the local user from the database.
