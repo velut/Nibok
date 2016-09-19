9a50d1e33554039fbcf3502a666428fa8074a42a
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-19 18:54:27 +0200

    Add Authenticator

---
+++ app/src/main/java/com/nibokapp/nibok/authentication/Authenticator.kt
@@ -0,0 +1,94 @@
+package com.nibokapp.nibok.authentication
+
+import android.util.Log
+import com.baasbox.android.BaasUser
+import com.baasbox.android.json.JsonArray
+import com.nibokapp.nibok.authentication.common.AuthenticatorInterface
+import com.nibokapp.nibok.data.repository.UserRepository
+import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
+import com.nibokapp.nibok.data.repository.server.ServerConstants
+import com.nibokapp.nibok.extension.getConversations
+import com.nibokapp.nibok.extension.getPublishedInsertions
+import com.nibokapp.nibok.extension.getSavedInsertions
+
+object Authenticator : AuthenticatorInterface {
+
+    const private val TAG = "Authenticator"
+
+    private val localUserRepository: UserRepositoryInterface = UserRepository
+
+
+    override fun signUp(username: String, password: String): Boolean {
+        Log.d(TAG, "Sign up user: $username")
+
+        val user = BaasUser.withUserName(username).setPassword(password)
+        user.apply {
+
+            getScope(BaasUser.Scope.PRIVATE)
+                    .put(ServerConstants.SAVED_INSERTIONS, JsonArray())
+                    .put(ServerConstants.CONVERSATIONS, JsonArray())
+
+            getScope(BaasUser.Scope.PUBLIC)
+                    .put(ServerConstants.PUBLISHED_INSERTIONS, JsonArray())
+                    .put(ServerConstants.AVATAR, "")
+        }
+
+        val result = user.signupSync()
+
+        if (result.isSuccess) {
+            val userData = result.value()
+            userData?.let {
+                localUserRepository.createLocalUser(it.name)
+            }
+            return true
+        } else {
+            return false
+        }
+    }
+
+    override fun login(username: String, password: String): Boolean {
+        Log.d(TAG, "Login user: $username")
+
+        val user = BaasUser.withUserName(username).setPassword(password)
+
+        val result = user.loginSync()
+
+        if (result.isSuccess) {
+            val userData = result.value()
+            userData?.let {
+                with(it) {
+                    localUserRepository
+                            .createLocalUser(name,
+                                    getSavedInsertions(),
+                                    getPublishedInsertions(),
+                                    getConversations())
+                }
+            }
+            return true
+        } else {
+            return false
+        }
+    }
+
+    override fun logout(): Boolean {
+        Log.d(TAG, "Logging out")
+        val currentUser = BaasUser.current()
+
+        if (currentUser == null) {
+            Log.d(TAG, "No current user to logout")
+            return false
+        }
+
+        val username = currentUser.name
+        val result = currentUser.logoutSync()
+
+        if (result.isSuccess) {
+            Log.d(TAG, "Logged out user: $username")
+            localUserRepository.removeLocalUser()
+            return true
+        } else {
+            Log.d(TAG, "Could not log out user: $username")
+            return false
+        }
+    }
+}
---
+++ app/src/main/java/com/nibokapp/nibok/authentication/common/AuthenticatorInterface.kt
@@ -0,0 +1,36 @@
+package com.nibokapp.nibok.authentication.common
+
+/**
+ * Interface for user authenticators.
+ */
+interface AuthenticatorInterface {
+
+    /**
+     * Sign up as a new user with the given username and password.
+     *
+     * @param username the user's unique username
+     * @param password the user's password
+     *
+     * @return true if the sign up was successful, false otherwise
+     */
+    fun signUp(username: String, password: String) : Boolean
+
+    /**
+     * Login as a returning user with the given username and password.
+     *
+     * @param username the user's unique username
+     * @param password the user's password
+     *
+     * @return true if the login was successful, false otherwise
+     */
+    fun login(username: String, password: String) : Boolean
+
+    /**
+     * Logout the currently logged in user if it exists.
+     *
+     * @return true if the user was correctly logged out, false otherwise
+     */
+    fun logout() : Boolean
+
+}
+
