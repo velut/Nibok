10990fbb11e5f0fc3c0181f8756f3925d9f55536
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-19 19:44:10 +0200

    Update UserModel and UserMapper

--- app/src/main/java/com/nibokapp/nibok/domain/mapper/user/UserMapper.kt
+++ app/src/main/java/com/nibokapp/nibok/domain/mapper/user/UserMapper.kt
@@ -10,16 +10,14 @@ class UserMapper() : UserMapperInterface {
 
     override fun convertUserToDomain(user: ExternalUser): UserModel = with(user) {
         UserModel(
-                id = id,
-                name = name,
+                username = username,
                 avatar = avatar
         )
     }
 
     override fun convertUserFromDomain(user: UserModel): ExternalUser = with(user) {
         ExternalUser(
-                id = id,
-                name = name,
+                username = username,
                 avatar = avatar
         )
     }
--- app/src/main/java/com/nibokapp/nibok/domain/model/UserModel.kt
+++ app/src/main/java/com/nibokapp/nibok/domain/model/UserModel.kt
@@ -3,12 +3,10 @@ package com.nibokapp.nibok.domain.model
 /**
  * Schema representing a user.
  *
- * @param id the id of the user
- * @param name the name of the user
+ * @param username the name of the user
  * @param avatar the source for the user's avatar
  */
 data class UserModel(
-        val id: Long,
-        val name: String,
+        val username: String,
         val avatar: String
 )
