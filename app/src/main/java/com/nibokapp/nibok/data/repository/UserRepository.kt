b505d6db958ae070b6978cdcc62f3e32a21f0ed5
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-19 18:19:58 +0200

    Add signUp and login; Update createLocalUser() and getLocalUserId()

--- app/src/main/java/com/nibokapp/nibok/data/repository/UserRepository.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/UserRepository.kt
@@ -1,6 +1,8 @@
 package com.nibokapp.nibok.data.repository
 
 import android.util.Log
+import com.nibokapp.nibok.data.db.Conversation
+import com.nibokapp.nibok.data.db.Insertion
 import com.nibokapp.nibok.data.db.User
 import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
 import com.nibokapp.nibok.extension.executeRealmTransaction
@@ -13,19 +15,35 @@ object UserRepository : UserRepositoryInterface {
 
     const private val TAG = "UserRepository"
 
-    override fun createLocalUser() {
+    override fun signUp(username: String, password: String) : Boolean {
+        throw UnsupportedOperationException("Sign up can only be done with the server")
+    }
+
+    override fun login(username: String, password: String): Boolean {
+        throw UnsupportedOperationException("Login can only be done with the server")
+    }
+
+    override fun createLocalUser(userId: String,
+                                 savedInsertions: List<Insertion>,
+                                 publishedInsertions: List<Insertion>,
+                                 conversations: List<Conversation>) {
         while (!localUserExists()) {
             Log.d(TAG, "Local user does not exist, trying to create one")
             executeRealmTransaction {
                 // Insert user in the db
                 val newUser = it.createObject(User::class.java)
-                newUser.id = 1 // TODO id from server/auth
+                newUser.apply {
+                    username = userId
+                    this.savedInsertions.addAll(savedInsertions)
+                    this.publishedInsertions.addAll(publishedInsertions)
+                    this.conversations.addAll(conversations)
+                }
             }
         }
         Log.d(TAG, "Local user created")
     }
 
-    override fun getLocalUserId(): Long = getLocalUser()?.id ?:
+    override fun getLocalUserId(): String = getLocalUser()?.username ?:
             throw IllegalStateException(
                     "Local user does not exist. Call createLocalUser() before retrieving data")
 
