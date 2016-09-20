ec528b7bc35fcb61ad15599d770218ace01a373e
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-20 20:00:08 +0200

    Add startConversation() to InsertionDetailPresenter

--- app/src/main/java/com/nibokapp/nibok/ui/presenter/InsertionDetailPresenter.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/presenter/InsertionDetailPresenter.kt
@@ -1,14 +1,19 @@
 package com.nibokapp.nibok.ui.presenter
 
 import android.util.Log
+import com.nibokapp.nibok.authentication.Authenticator
+import com.nibokapp.nibok.authentication.common.AuthenticatorInterface
 import com.nibokapp.nibok.domain.command.bookinsertion.RequestBookInsertionByIdCommand
+import com.nibokapp.nibok.domain.command.conversation.StartConversationCommand
 import com.nibokapp.nibok.domain.command.user.RequestLocalUserIdCommand
 import com.nibokapp.nibok.domain.model.BookInsertionModel
 
 /**
  * Presenter that retrieves full details about an insertion.
  */
-class InsertionDetailPresenter {
+class InsertionDetailPresenter(
+        val authenticator: AuthenticatorInterface = Authenticator
+) {
 
     companion object {
         private val TAG = InsertionDetailPresenter::class.java.simpleName
@@ -25,6 +30,14 @@ class InsertionDetailPresenter {
     }
 
     /**
+     * Check if there is a currently logged in user or not.
+     *
+     * @return true if there is a currently logged in user, false otherwise
+     */
+    fun loggedUserExists() : Boolean =
+            authenticator.currentUserExists()
+
+    /**
      * Get detailed data about the insertion with the given id.
      *
      * @param insertionId the id of the insertion
@@ -34,5 +47,18 @@ class InsertionDetailPresenter {
     fun getInsertionDetails(insertionId: String) : BookInsertionModel? {
         Log.d(TAG, "Getting book insertion $insertionId details")
         return RequestBookInsertionByIdCommand(insertionId).execute()
+    }
+
+    /**
+     * Start a conversation with the partner with the given id.
+     *
+     * @param partnerId the id of the partner
+     *
+     * @return the id of the started conversation
+     * or null if the conversation could not be started
+     */
+    fun startConversation(partnerId: String) : String? {
+        Log.d(TAG, "Starting conversation with: $partnerId")
+        return StartConversationCommand(partnerId).execute()
     }
 }
