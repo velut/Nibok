b6d81d62e14ed512cbdb18a970274e943801c1f7
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-20 20:53:30 +0200

    Move extensions from BookInsertionRepository to RepositoryExtensions

--- app/src/main/java/com/nibokapp/nibok/extension/RepositoryExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/RepositoryExtensions.kt
@@ -1,5 +1,10 @@
 package com.nibokapp.nibok.extension
 
+import com.nibokapp.nibok.data.db.Insertion
+import com.nibokapp.nibok.data.repository.BookInsertionRepository
+import com.nibokapp.nibok.data.repository.UserRepository
+import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
+
 /**
  * Extensions for the base repository
  */
@@ -17,4 +22,38 @@ inline fun <T, R: Any> Iterable<T>.firstResultOrNull(predicate: (T) -> R?) : R? 
         if (result != null) return result
     }
     return null
+}
+
+/**
+ * When a local user exists exclude his insertions from this list.
+ */
+fun List<Insertion>.excludeUserOwnInsertions() : List<Insertion> {
+    val userRepository: UserRepositoryInterface = UserRepository
+    if (!userRepository.localUserExists()) {
+        return this
+    } else {
+        val userId = userRepository.getLocalUserId()
+        return this.filter { it.seller?.username != userId }
+    }
+}
+
+/**
+ * When a local user exists include only his insertions from this list.
+ */
+fun List<Insertion>.includeOnlyUserOwnInsertions() : List<Insertion> {
+    val userRepository: UserRepositoryInterface = UserRepository
+    if (!userRepository.localUserExists()) {
+        return this
+    } else {
+        val userId = userRepository.getLocalUserId()
+        return this.filter { it.seller?.username == userId }
+    }
+}
+
+/**
+ * Include only saved insertions from this list.
+ */
+fun List<Insertion>.includeOnlySavedInsertions() : List<Insertion> {
+    val savedInsertions = BookInsertionRepository.getSavedBookInsertionList()
+    return this.filter { it.id in savedInsertions.map { it.id } }
 }
