753fa6e03e1e0a83382109dea7d73423ee443ede
Edoardo Scibona <velut@users.noreply.github.com>
2016-09-21 22:02:13 +0200

    Add buildDocumentFromInsertion() and fetchBookDataFromISBN() extensions

--- app/src/main/java/com/nibokapp/nibok/extension/ServerRepositoryExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/ServerRepositoryExtensions.kt
@@ -2,18 +2,20 @@ package com.nibokapp.nibok.extension
 
 import com.baasbox.android.BaasDocument
 import com.baasbox.android.BaasQuery
+import com.baasbox.android.BaasResult
 import com.baasbox.android.BaasUser
 import com.baasbox.android.json.JsonArray
 import com.nibokapp.nibok.data.db.Book
 import com.nibokapp.nibok.data.db.Conversation
 import com.nibokapp.nibok.data.db.ExternalUser
 import com.nibokapp.nibok.data.db.Insertion
+import com.nibokapp.nibok.data.repository.server.common.ServerConstants
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.AUTHORS
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.AVATAR
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.BOOK_CONDITION
+import com.nibokapp.nibok.data.repository.server.common.ServerConstants.BOOK_ID
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.BOOK_PICTURES
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.BOOK_PRICE
-import com.nibokapp.nibok.data.repository.server.common.ServerConstants.COLLECTION_BOOKS
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.COLLECTION_CONVERSATIONS
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.COLLECTION_INSERTIONS
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.CONVERSATIONS
@@ -21,6 +23,7 @@ import com.nibokapp.nibok.data.repository.server.common.ServerConstants.ISBN
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.PUBLISHED_INSERTIONS
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.PUBLISHER
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.SAVED_INSERTIONS
+import com.nibokapp.nibok.data.repository.server.common.ServerConstants.SELLER_ID
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.TITLE
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants.YEAR
 
@@ -73,6 +76,24 @@ fun buildInsertionFromDocument(document: BaasDocument): Insertion {
     }
 }
 
+fun buildDocumentFromInsertion(insertion: Insertion, bookId: String): BaasDocument {
+    return with(insertion) {
+
+        val document = BaasDocument(COLLECTION_INSERTIONS)
+
+        val picturesArray = JsonArray()
+        bookImagesSources.toStringList().forEach { picturesArray.add(it) }
+
+        document.apply {
+            put(SELLER_ID, seller!!.username)
+            put(BOOK_ID, bookId)
+            put(BOOK_PRICE, bookPrice.toString())
+            put(BOOK_CONDITION, bookCondition)
+            put(BOOK_PICTURES, picturesArray)
+        }
+    }
+}
+
 fun JsonArray.fetchConversations() : List<Conversation> {
     this.filterIsInstance<String>()
             .map { BaasDocument.fetchSync(COLLECTION_CONVERSATIONS, it) }
@@ -109,13 +130,8 @@ fun fetchUserFromId(username: String) : ExternalUser? {
 }
 
 fun fetchBookFromISBN(isbn: String): Book? {
-    val query = BaasQuery.builder()
-            .collection(COLLECTION_BOOKS)
-            .where("isbn=?")
-            .whereParams(isbn)
-            .build()
+    val result = fetchBookDataFromISBN(isbn)
 
-    val result = query.querySync()
     if (result.isSuccess && result.value() != null) {
         val bookData = result.value()
 
@@ -133,4 +149,13 @@ fun fetchBookFromISBN(isbn: String): Book? {
     } else {
         return null
     }
+}
+
+fun fetchBookDataFromISBN(isbn: String): BaasResult<MutableList<BaasDocument>> {
+    val query = BaasQuery.builder()
+            .where("$ISBN=$isbn")
+            .criteria()
+
+    val result = BaasDocument.fetchAllSync(ServerConstants.COLLECTION_BOOKS, query)
+    return result
 }
