31a7576260931b6113b0c840858497cb8d67717f
Edoardo Scibona <velut@users.noreply.github.com>
2016-10-14 20:24:59 +0200

    Refactor Authenticator using Baas extension functions

--- app/src/main/java/com/nibokapp/nibok/authentication/Authenticator.kt
+++ app/src/main/java/com/nibokapp/nibok/authentication/Authenticator.kt
@@ -4,15 +4,16 @@ import android.util.Log
 import com.baasbox.android.BaasBox
 import com.baasbox.android.BaasUser
 import com.baasbox.android.Rest
-import com.baasbox.android.json.JsonArray
 import com.nibokapp.nibok.authentication.common.AuthenticatorInterface
 import com.nibokapp.nibok.data.repository.UserRepository
 import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants
-import com.nibokapp.nibok.extension.getConversations
-import com.nibokapp.nibok.extension.getPublishedInsertions
-import com.nibokapp.nibok.extension.getSavedInsertions
+import com.nibokapp.nibok.extension.*
 
+/**
+ * Authenticator.
+ * This object is responsible for authentication related operations.
+ */
 object Authenticator : AuthenticatorInterface {
 
     const private val TAG = "Authenticator"
@@ -24,27 +25,10 @@ object Authenticator : AuthenticatorInterface {
         Log.d(TAG, "Sign up user: $username")
 
         val user = BaasUser.withUserName(username).setPassword(password)
-        user.apply {
+        user.init()
 
-            getScope(BaasUser.Scope.PRIVATE)
-                    .put(ServerConstants.SAVED_INSERTIONS, JsonArray())
-                    .put(ServerConstants.CONVERSATIONS, JsonArray())
-
-            getScope(BaasUser.Scope.PUBLIC)
-                    .put(ServerConstants.PUBLISHED_INSERTIONS, JsonArray())
-                    .put(ServerConstants.AVATAR, "")
-        }
-
-        val result = user.signupSync()
-
-        if (result.isSuccess) {
-            val userData = result.value()
-            userData?.let {
-                localUserRepository.createLocalUser(it.name)
-            }
-            return true
-        } else {
-            return false
+        return user.signupSync().onSuccess {
+            localUserRepository.createLocalUser(it.name)
         }
     }
 
@@ -53,22 +37,14 @@ object Authenticator : AuthenticatorInterface {
 
         val user = BaasUser.withUserName(username).setPassword(password)
 
-        val result = user.loginSync()
-
-        if (result.isSuccess) {
-            val userData = result.value()
-            userData?.let {
-                with(it) {
-                    localUserRepository
-                            .createLocalUser(name,
-                                    getSavedInsertions(),
-                                    getPublishedInsertions(),
-                                    getConversations())
-                }
+        return user.loginSync().onSuccess {
+            with(it) {
+                localUserRepository
+                        .createLocalUser(name,
+                                getSavedInsertions(),
+                                getPublishedInsertions(),
+                                getConversations())
             }
-            return true
-        } else {
-            return false
         }
     }
 
@@ -82,37 +58,32 @@ object Authenticator : AuthenticatorInterface {
         }
 
         val username = currentUser.name
-        val result = currentUser.logoutSync()
 
-        if (result.isSuccess) {
-            Log.d(TAG, "Logged out user: $username")
+        return currentUser.logoutSync().onSuccess {
             localUserRepository.removeLocalUser()
-            return true
-        } else {
-            Log.d(TAG, "Could not log out user: $username")
-            return false
+            Log.d(TAG, "Logged out user: $username")
         }
     }
 
     override fun currentUserExists(): Boolean = BaasUser.current() != null
 
     override fun isUsernameAvailable(username: String): Boolean {
-        val rest = BaasBox.rest()
 
-        val result = rest.sync(
+        val result = BaasBox.rest().sync(
                 Rest.Method.GET,
                 "plugin/users.usernameAvailable?username=$username"
         )
 
-        if (result.isSuccess) {
-            val data = result.value() ?: return true
-            val available =
-                    data.getString(ServerConstants.DATA) == ServerConstants.USERNAME_AVAILABLE
-            Log.d(TAG, "Username: $username available: $available")
-            return available
-        } else {
-            Log.d(TAG, "Username check fail")
-            return true
+        /*
+         * Initially we assume that the username is available,
+         * this is done so that in the case the request fails
+         * we don't make the strong assumption that the username
+         * is already taken.
+         */
+        var available = true
+        result.onSuccess {
+            available = it.getString(ServerConstants.DATA) == ServerConstants.USERNAME_AVAILABLE
         }
+        return available
     }
 }
