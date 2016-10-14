bba51dc5bb296a207354f1e1ea05f6faf952e93f
Edoardo Scibona <velut@users.noreply.github.com>
2016-10-14 20:11:35 +0200

    Add BaasUser extensions: init(), getPrivateScope(), getPublicScope()

--- app/src/main/java/com/nibokapp/nibok/extension/BaasExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/BaasExtensions.kt
@@ -1,6 +1,10 @@
 package com.nibokapp.nibok.extension
 
 import com.baasbox.android.BaasResult
+import com.baasbox.android.BaasUser
+import com.baasbox.android.json.JsonArray
+import com.baasbox.android.json.JsonObject
+import com.nibokapp.nibok.data.repository.server.common.ServerConstants
 
 /**
  * Extensions handling Baas related operations.
@@ -22,3 +26,43 @@ inline fun <reified T> BaasResult<T>.onSuccess(func: (T) -> Unit) : Boolean {
     }
     return false
 }
+
+/*
+ * BAAS USER
+ */
+
+/**
+ * Initialize a new BaasUser by putting empty collections in the fields
+ * relevant to the platform.
+ */
+fun BaasUser.init() {
+    this.apply {
+        with(ServerConstants) {
+
+            getPrivateScope()?.apply {
+                put(SAVED_INSERTIONS, JsonArray())
+                put(CONVERSATIONS, JsonArray())
+            }
+
+            getPublicScope()?.apply {
+                put(PUBLISHED_INSERTIONS, JsonArray())
+                put(AVATAR, "")
+            }
+
+        }
+    }
+}
+
+/**
+ * Get the private scope of this BaasUser.
+ *
+ * @return the private scope if the current user has access to it, null otherwise
+ */
+fun BaasUser.getPrivateScope(): JsonObject? = this.getScope(BaasUser.Scope.PRIVATE)
+
+/**
+ * Get the public scope of this BaasUser.
+ *
+ * @return the private scope if the current user has access to it, null otherwise
+ */
+fun BaasUser.getPublicScope(): JsonObject? = this.getScope(BaasUser.Scope.PUBLIC)
