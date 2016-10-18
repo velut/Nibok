a2472c59dba6b1c6fbc67aae228bfb8e4d2328ae
Edoardo Scibona <velut@users.noreply.github.com>
2016-10-18 19:21:23 +0200

    Refactor ServerBookInsertionRepository using fetcher and mapper

--- app/src/main/java/com/nibokapp/nibok/data/repository/server/ServerBookInsertionRepository.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/server/ServerBookInsertionRepository.kt
@@ -1,16 +1,20 @@
 package com.nibokapp.nibok.data.repository.server
 
 import android.util.Log
-import com.baasbox.android.BaasDocument
-import com.baasbox.android.BaasQuery
 import com.baasbox.android.BaasUser
 import com.baasbox.android.BaasUser.Scope
+import com.baasbox.android.BaasUser.current
 import com.baasbox.android.json.JsonArray
 import com.nibokapp.nibok.data.db.Book
 import com.nibokapp.nibok.data.db.Insertion
 import com.nibokapp.nibok.data.repository.common.BookInsertionRepositoryInterface
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants
-import com.nibokapp.nibok.extension.*
+import com.nibokapp.nibok.extension.getPublishedInsertions
+import com.nibokapp.nibok.extension.getSavedInsertions
+import com.nibokapp.nibok.server.fetch.ServerDataFetcher
+import com.nibokapp.nibok.server.fetch.common.ServerDataFetcherInterface
+import com.nibokapp.nibok.server.mapper.ServerDataMapper
+import com.nibokapp.nibok.server.mapper.common.ServerDataMapperInterface
 import java.util.*
 
 /**
@@ -18,163 +22,122 @@ import java.util.*
  */
 object ServerBookInsertionRepository: BookInsertionRepositoryInterface {
 
-    const private val TAG = "ServerBookInsertionRepository"
+    const private val TAG = "ServerBookInsertionRepo"
 
-    private var feedCache : List<Insertion> = emptyList()
-    private var savedCache : List<Insertion> = emptyList()
-    private var publishedCache : List<Insertion> = emptyList()
+    // Fetcher and mapper used to retrieve data from the server
+    // and map it into data for the local db
+    private val fetcher: ServerDataFetcherInterface = ServerDataFetcher()
+    private val mapper: ServerDataMapperInterface = ServerDataMapper()
+
+    // Current logged in user
+    private val currentUser: BaasUser?
+        get() = current()
+
+    // Caches
+    private var feedCache: List<Insertion> = emptyList()
+    private var savedCache: List<Insertion> = emptyList()
+    private var publishedCache: List<Insertion> = emptyList()
 
     /*
      * COMMON FUNCTIONS
      */
 
-    override fun getBookInsertionById(insertionId: String) : Insertion? {
-        val result = BaasDocument.fetchSync(ServerConstants.COLLECTION_INSERTIONS, insertionId)
-        if (result.isSuccess && result.value() != null) {
-            val insertion = buildInsertionFromDocument(result.value())
-            return insertion
-        } else {
-            return null
-        }
+    override fun getInsertionById(insertionId: String) : Insertion? {
+        val result = fetcher.fetchInsertionDocumentById(insertionId)
+        return result?.let { mapper.convertDocumentToInsertion(it) }
     }
 
     override fun getBookByISBN(isbn: String): Book? {
-        return fetchBookFromISBN(isbn)
+        val result = fetcher.fetchBookDocumentFromISBN(isbn)
+        return result?.let { mapper.convertDocumentToBook(it) }
     }
 
-    override fun getBookInsertionListFromQuery(query: String) : List<Insertion> {
-
-        val trimmedQuery = query.trim()
-
-        if (trimmedQuery.isEmpty()) return emptyList()
-
-        val whereString =
-                "${ServerConstants.TITLE} like $query or " +
-                "$query in ${ServerConstants.AUTHORS} or " +
-                "${ServerConstants.PUBLISHER} like $query or " +
-                "${ServerConstants.ISBN} like $query"
-
-        val serverQuery = BaasQuery.builder()
-                .where(whereString)
-                .criteria()
-
-        val result = BaasDocument.fetchAllSync(ServerConstants.COLLECTION_INSERTIONS, serverQuery)
-        if (result.isSuccess && result.value() != null) {
-            val insertions = result.value().map { buildInsertionFromDocument(it) }
-            Log.d(TAG, "Book insertions corresponding to query '$query' = ${insertions.size}")
-            return insertions
-        } else {
-            return emptyList()
-        }
+    override fun getInsertionListFromQuery(query: String) : List<Insertion> {
+        val result = fetcher.fetchInsertionDocumentListByQuery(query)
+        val insertions = mapper.convertDocumentListToInsertions(result)
+        Log.d(TAG, "Found ${insertions.size} insertions corresponding to query: $query")
+        return insertions
     }
 
-    override fun getBookInsertionListAfterDate(date: Date) : List<Insertion> {
-
-        val whereString =
-                "${ServerConstants.DATE} >= ${date.toStringDate()}"
-
-        val serverQuery = BaasQuery.builder()
-                .where(whereString)
-                .criteria()
-
-        val result = BaasDocument.fetchAllSync(ServerConstants.COLLECTION_INSERTIONS, serverQuery)
-        if (result.isSuccess && result.value() != null) {
-            val insertions = result.value().map { buildInsertionFromDocument(it) }
-            Log.d(TAG, "Found ${insertions.size} insertions after $date")
-            return insertions
-        } else {
-            return emptyList()
-        }
+    override fun getInsertionListAfterDate(date: Date) : List<Insertion> {
+        val result = fetcher.fetchInsertionDocumentListAfterDate(date)
+        val insertions = mapper.convertDocumentListToInsertions(result)
+        Log.d(TAG, "Found ${insertions.size} insertions after date: $date")
+        return insertions
     }
 
-    override fun getBookInsertionListBeforeDate(date: Date) : List<Insertion> {
-        val whereString =
-                "${ServerConstants.DATE} <= ${date.toStringDate()}"
-
-        val serverQuery = BaasQuery.builder()
-                .where(whereString)
-                .criteria()
-
-        val result = BaasDocument.fetchAllSync(ServerConstants.COLLECTION_INSERTIONS, serverQuery)
-        if (result.isSuccess && result.value() != null) {
-            val insertions = result.value().map { buildInsertionFromDocument(it) }
-            Log.d(TAG, "Found ${insertions.size} insertions before $date")
-            return insertions
-        } else {
-            return emptyList()
-        }
+    override fun getInsertionListBeforeDate(date: Date) : List<Insertion> {
+        val result = fetcher.fetchInsertionDocumentListBeforeDate(date)
+        val insertions = mapper.convertDocumentListToInsertions(result)
+        Log.d(TAG, "Found ${insertions.size} insertions before date: $date")
+        return insertions
     }
 
     /*
      * FEED BOOK INSERTIONS
      */
 
-    override fun getFeedBookInsertionList(cached: Boolean): List<Insertion> {
+    override fun getFeedInsertionList(cached: Boolean): List<Insertion> {
         if (cached) return feedCache
-        val result = BaasDocument.fetchAllSync(ServerConstants.COLLECTION_INSERTIONS)
-        if (result.isSuccess && result.value() != null) {
-            feedCache = result.value().map { buildInsertionFromDocument(it) }
-                    .excludeUserOwnInsertions()
-            Log.d(TAG, "Found ${feedCache.size} feed insertions")
-            return feedCache
-        } else {
-            return emptyList()
-        }
+        val result = fetcher.fetchRecentInsertionDocumentList()
+        feedCache = mapper.convertDocumentListToInsertions(result).excludeUserOwnInsertions()
+        Log.d(TAG, "Found ${feedCache.size} feed insertions")
+        return feedCache
     }
 
-    override fun getFeedBookInsertionListFromQuery(query: String) : List<Insertion>  =
-            getBookInsertionListFromQuery(query).excludeUserOwnInsertions()
+    override fun getFeedInsertionListFromQuery(query: String) : List<Insertion>  =
+            getInsertionListFromQuery(query).excludeUserOwnInsertions()
 
-    override fun getFeedBookInsertionListAfterDate(date: Date) : List<Insertion> =
-            getBookInsertionListAfterDate(date).excludeUserOwnInsertions()
+    override fun getFeedInsertionListAfterDate(date: Date) : List<Insertion> =
+            getInsertionListAfterDate(date).excludeUserOwnInsertions()
 
-    override fun getFeedBookInsertionListBeforeDate(date: Date) : List<Insertion> =
-            getBookInsertionListBeforeDate(date).excludeUserOwnInsertions()
+    override fun getFeedInsertionListBeforeDate(date: Date) : List<Insertion> =
+            getInsertionListBeforeDate(date).excludeUserOwnInsertions()
 
     /*
      * SAVED BOOK INSERTIONS
      */
 
-    override fun getSavedBookInsertionList(cached: Boolean) : List<Insertion> {
+    override fun getSavedInsertionList(cached: Boolean) : List<Insertion> {
         if (cached) return savedCache
-        savedCache = BaasUser.current()?.getSavedInsertions() ?: emptyList()
+        savedCache = currentUser?.getSavedInsertions() ?: emptyList()
         return savedCache
     }
 
-    override fun getSavedBookInsertionListFromQuery(query: String) : List<Insertion> =
-            getBookInsertionListFromQuery(query).includeOnlySavedInsertions()
+    override fun getSavedInsertionListFromQuery(query: String) : List<Insertion> =
+            getInsertionListFromQuery(query).includeOnlySavedInsertions()
 
-    override fun getSavedBookInsertionLisAfterDate(date: Date) : List<Insertion> =
-            getBookInsertionListAfterDate(date).includeOnlySavedInsertions()
+    override fun getSavedInsertionLisAfterDate(date: Date) : List<Insertion> =
+            getInsertionListAfterDate(date).includeOnlySavedInsertions()
 
-    override fun getSavedBookInsertionListBeforeDate(date: Date) : List<Insertion> =
-            getBookInsertionListBeforeDate(date).includeOnlySavedInsertions()
+    override fun getSavedInsertionListBeforeDate(date: Date) : List<Insertion> =
+            getInsertionListBeforeDate(date).includeOnlySavedInsertions()
 
     /*
      * PUBLISHED BOOK INSERTIONS
      */
 
-    override fun getPublishedBookInsertionList(cached: Boolean) : List<Insertion> {
+    override fun getPublishedInsertionList(cached: Boolean) : List<Insertion> {
         if (cached) return publishedCache
-        publishedCache = BaasUser.current()?.getPublishedInsertions() ?: emptyList()
+        publishedCache = currentUser?.getPublishedInsertions() ?: emptyList()
         return publishedCache
     }
 
-    override fun getPublishedBookInsertionListFromQuery(query: String) : List<Insertion> =
-            getBookInsertionListFromQuery(query).includeOnlyUserOwnInsertions()
+    override fun getPublishedInsertionListFromQuery(query: String) : List<Insertion> =
+            getInsertionListFromQuery(query).includeOnlyUserOwnInsertions()
 
-    override fun getPublishedBookInsertionListAfterDate(date: Date) : List<Insertion> =
-            getBookInsertionListAfterDate(date).includeOnlyUserOwnInsertions()
+    override fun getPublishedInsertionListAfterDate(date: Date) : List<Insertion> =
+            getInsertionListAfterDate(date).includeOnlyUserOwnInsertions()
 
-    override fun getPublishedBookInsertionListBeforeDate(date: Date) : List<Insertion> =
-            getBookInsertionListBeforeDate(date).includeOnlyUserOwnInsertions()
+    override fun getPublishedInsertionListBeforeDate(date: Date) : List<Insertion> =
+            getInsertionListBeforeDate(date).includeOnlyUserOwnInsertions()
 
     /*
      * BOOK INSERTION SAVE STATUS
      */
 
     override fun isBookInsertionSaved(insertionId: String) : Boolean =
-            insertionId in getSavedBookInsertionList().map { it.id }
+            insertionId in getSavedInsertionList().map { it.id }
 
     override fun toggleBookInsertionSaveStatus(insertionId: String) : Boolean {
         val user = BaasUser.current() ?:
@@ -220,7 +183,7 @@ object ServerBookInsertionRepository: BookInsertionRepositoryInterface {
     }
 
     private fun List<Insertion>.includeOnlySavedInsertions() : List<Insertion> {
-        val savedInsertions = getSavedBookInsertionList()
+        val savedInsertions = getSavedInsertionList()
         return this.filter { it.id in savedInsertions.map { it.id } }
     }
 }
