66ff903b6c3ca834d0fdff6173b1bff8cdb36b08
Edoardo Scibona <velut@users.noreply.github.com>
2016-10-18 18:53:16 +0200

    Refactor and implement methods in ServerDataFetcher

--- app/src/main/java/com/nibokapp/nibok/server/fetch/ServerDataFetcher.kt
+++ app/src/main/java/com/nibokapp/nibok/server/fetch/ServerDataFetcher.kt
@@ -7,12 +7,23 @@ import com.baasbox.android.json.JsonArray
 import com.nibokapp.nibok.data.repository.server.common.ServerCollection
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants
 import com.nibokapp.nibok.extension.onSuccessReturn
+import com.nibokapp.nibok.extension.toStringDate
 import com.nibokapp.nibok.server.fetch.common.ServerDataFetcherInterface
+import java.util.*
 
 /**
  * ServerDataFetcher implements the ServerDataFetcherInterface and fetches data from the server.
  */
 class ServerDataFetcher : ServerDataFetcherInterface {
+
+    companion object {
+        private val COLL_INSERTIONS = ServerCollection.INSERTIONS
+        private val COLL_BOOKS = ServerCollection.BOOKS
+        private val COLL_CONVERSATIONS = ServerCollection.CONVERSATIONS
+        private val COLL_MESSAGES = ServerCollection.MESSAGES
+
+        private val RECORDS_PER_PAGE = 30
+    }
 
     /*
      * USER
@@ -28,46 +39,115 @@ class ServerDataFetcher : ServerDataFetcherInterface {
      */
 
     override fun fetchBookDocumentFromISBN(isbn: String) : BaasDocument? {
-        val query = BaasQuery.builder()
-                .where("${ServerConstants.ISBN}=$isbn")
-                .criteria()
-
-        val result = BaasDocument.fetchAllSync(ServerCollection.BOOKS.id, query)
-
-        val book = result.onSuccessReturn { it.getOrNull(0) }
-        return book
+        val whereString = "${ServerConstants.ISBN}=$isbn"
+        val bookDocList = queryDocumentListFromCollection(COLL_BOOKS, whereString)
+        return bookDocList.getOrNull(0) // Get the first book in the list
     }
 
     /*
      * INSERTIONS
      */
 
-    override fun fetchInsertionDocuments(idsArray: JsonArray) : List<BaasDocument> {
-        return fetchDocumentsFromCollection(idsArray, ServerCollection.INSERTIONS)
+    override fun fetchRecentInsertionDocumentList(): List<BaasDocument> {
+        return fetchRecentDocumentListFromCollection(COLL_INSERTIONS)
     }
 
-    /*
+    override fun fetchInsertionDocumentListById(idsArray: JsonArray) : List<BaasDocument> {
+        return fetchDocumentListFromCollectionById(idsArray, COLL_INSERTIONS)
+    }
+
+    override fun fetchInsertionDocumentById(id: String): BaasDocument? {
+        val result = BaasDocument.fetchSync(COLL_INSERTIONS.id, id)
+        return result.onSuccessReturn { it }
+    }
+
+    override fun fetchInsertionDocumentListByQuery(query: String): List<BaasDocument> {
+        val trimmedQuery = query.trim()
+
+        if (trimmedQuery.isEmpty()) return emptyList()
+
+        val whereString = with(ServerConstants) {
+                "$TITLE like $query or " +
+                "$query in $AUTHORS or " +
+                "$PUBLISHER like $query or " +
+                "$ISBN like $query"
+        }
+        return queryDocumentListFromCollection(COLL_INSERTIONS, whereString)
+    }
+
+    override fun fetchInsertionDocumentListAfterDate(date: Date): List<BaasDocument> {
+        val whereString = "${ServerConstants.DATE} >= ${date.toStringDate()}"
+        return queryDocumentListFromCollection(COLL_INSERTIONS, whereString)
+    }
+
+    override fun fetchInsertionDocumentListBeforeDate(date: Date): List<BaasDocument> {
+        val whereString = "${ServerConstants.DATE} <= ${date.toStringDate()}"
+        return queryDocumentListFromCollection(COLL_INSERTIONS, whereString)
+    }
+/*
      * CONVERSATIONS
      */
 
-    override fun fetchConversationDocuments(idsArray: JsonArray) : List<BaasDocument> {
-        return fetchDocumentsFromCollection(idsArray, ServerCollection.CONVERSATIONS)
+    override fun fetchConversationDocumentListById(idsArray: JsonArray) : List<BaasDocument> {
+        return fetchDocumentListFromCollectionById(idsArray, COLL_CONVERSATIONS)
     }
 
     /*
      * MESSAGES
      */
 
-    override fun fetchMessageDocuments(idsArray: JsonArray) : List<BaasDocument> {
-        return fetchDocumentsFromCollection(idsArray, ServerCollection.MESSAGES)
+    override fun fetchMessageDocumentList(idsArray: JsonArray) : List<BaasDocument> {
+        return fetchDocumentListFromCollectionById(idsArray, COLL_MESSAGES)
     }
 
-    private fun fetchDocumentsFromCollection(idsArray: JsonArray, collection: ServerCollection) :
-            List<BaasDocument> {
+    /**
+     * Fetch recent documents from the server belonging to the specified collection.
+     * The returned list of document is ordered by date.
+     *
+     * @param collection the collection from which the documents are fetched
+     *
+     * @return a list of BaasDocument
+     */
+    private fun fetchRecentDocumentListFromCollection(collection: ServerCollection)
+            : List<BaasDocument>{
+        val criteria = BaasQuery.builder()
+                .pagination(0, RECORDS_PER_PAGE)
+                .orderBy(ServerConstants.DATE)
+                .criteria()
+        val result = BaasDocument.fetchAllSync(collection.id, criteria)
+        return result.onSuccessReturn { it } ?: emptyList()
+    }
+
+    /**
+     * Fetch the list of documents with the ids given in the array from the specified collection.
+     *
+     * @param idsArray the array containing the ids of the documents to fetch
+     * @param collection the collection from which the documents are fetched
+     *
+     * @return a list of BaasDocument
+     */
+    private fun fetchDocumentListFromCollectionById(idsArray: JsonArray,
+                                                    collection: ServerCollection)
+            : List<BaasDocument> {
         return idsArray.filterIsInstance<String>() // Get the strings representing documents ids
                 .map { BaasDocument.fetchSync(collection.id, it) } // Fetch docs from the collection
                 .filter { it.isSuccess } // Keep only successful requests
                 .map { it.value() } // Extract the BaasDocument
                 .filterNotNull() // Filter eventually null values
+    }
+
+    /**
+     * Query documents from the specified collection matching the given where clause.
+     *
+     * @param collection the collection from which the documents are fetched
+     * @param whereConditions the String representing the where clause
+     *
+     * @return a list of BaasDocument
+     */
+    private fun queryDocumentListFromCollection(collection: ServerCollection,
+                                                whereConditions: String) : List<BaasDocument> {
+        val criteria = BaasQuery.builder().where(whereConditions).criteria()
+        val result = BaasDocument.fetchAllSync(collection.id, criteria)
+        return result.onSuccessReturn { it } ?: emptyList()
     }
 }
