b753e55b2a434f7a8cdc6941a19fd122db5a1f1f
Edoardo Scibona <velut@users.noreply.github.com>
2016-10-19 20:21:41 +0200

    Update logout() to work offline by always deleting the local user and then dispatching an async logout call to the server

--- app/src/main/java/com/nibokapp/nibok/authentication/Authenticator.kt
+++ app/src/main/java/com/nibokapp/nibok/authentication/Authenticator.kt
@@ -9,6 +9,7 @@ import com.nibokapp.nibok.data.repository.UserRepository
 import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
 import com.nibokapp.nibok.data.repository.server.common.ServerConstants
 import com.nibokapp.nibok.extension.*
+import org.jetbrains.anko.doAsync
 
 /**
  * Authenticator.
@@ -19,6 +20,9 @@ object Authenticator : AuthenticatorInterface {
     const private val TAG = "Authenticator"
 
     private val localUserRepository: UserRepositoryInterface = UserRepository
+
+    private val loggedUser: BaasUser?
+        get() = BaasUser.current()
 
 
     override fun signUp(username: String, password: String): Boolean {
@@ -50,28 +54,29 @@ object Authenticator : AuthenticatorInterface {
 
     override fun logout(): Boolean {
         Log.d(TAG, "Logging out")
-        val currentUser = BaasUser.current()
 
-        if (currentUser == null) {
-            Log.d(TAG, "No current user to logout")
-            return false
+        // Remove the local user, even if logout from the server will be unsuccessful
+        Log.d(TAG, "Removing eventual local user")
+        localUserRepository.removeLocalUser()
+
+        // Try to log out user from the server.
+        // Logout may fail if the server is unreachable.
+        // Async in order to let the caller go on not waiting for the logout
+        Log.d(TAG, "Trying to logout user from the server")
+        loggedUser?.let {
+            doAsync {
+                val loggedOut = it.logoutSync().isSuccess
+                Log.d(TAG, if (loggedOut) "Logged out"
+                            else "Could not log out" + " user: ${it.name}")
+            }
         }
 
-        val username = currentUser.name
-
-        val loggedOut = currentUser.logoutSync().isSuccess
-
-        if (loggedOut) {
-            localUserRepository.removeLocalUser()
-            Log.d(TAG, "Logged out user: $username")
-        } else {
-            Log.d(TAG, "Could not logout user: $username")
-        }
-
-        return loggedOut
+        // Return true as the local user does not exist anymore
+        // and a failed server logout does not impact a successive login
+        return true
     }
 
-    override fun currentUserExists(): Boolean = BaasUser.current() != null
+    override fun currentUserExists(): Boolean = localUserExists()
 
     override fun isUsernameAvailable(username: String): Boolean {
 
@@ -91,4 +96,6 @@ object Authenticator : AuthenticatorInterface {
         }
         return available ?: true
     }
+
+    private fun localUserExists() : Boolean = localUserRepository.localUserExists()
 }
