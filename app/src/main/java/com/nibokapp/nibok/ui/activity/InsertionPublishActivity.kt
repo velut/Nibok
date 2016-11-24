e800ef83b881001349b4090c4dd8474e39a731f2
Edoardo Scibona <velut@users.noreply.github.com>
2016-11-24 19:06:47 +0100

    Add book data retrieval when setting isbn

--- app/src/main/java/com/nibokapp/nibok/ui/activity/InsertionPublishActivity.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/activity/InsertionPublishActivity.kt
@@ -15,12 +15,17 @@ import com.nibokapp.nibok.domain.model.publish.BookData
 import com.nibokapp.nibok.domain.model.publish.InsertionData
 import com.nibokapp.nibok.ui.fragment.publish.*
 import com.nibokapp.nibok.ui.fragment.publish.common.BasePublishFragment
+import com.nibokapp.nibok.ui.presenter.PublishInsertionPresenter
 import kotlinx.android.synthetic.main.activity_insertion_publish.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 /**
  * InsertionPublishActivity manages insertion publishing.
  */
-class InsertionPublishActivity : AppCompatActivity(), BasePublishFragment.PublishProcessManager {
+class InsertionPublishActivity(
+        private val presenter: PublishInsertionPresenter = PublishInsertionPresenter()
+) : AppCompatActivity(), BasePublishFragment.PublishProcessManager {
 
     companion object {
         private val TAG = InsertionPublishActivity::class.java.simpleName
@@ -39,19 +44,19 @@ class InsertionPublishActivity : AppCompatActivity(), BasePublishFragment.Publis
     }
 
     private var alertQuitDialog: MaterialDialog? = null
-
-    private val dialogs: List<MaterialDialog?> = listOf(alertQuitDialog)
+    private var progressDialog: MaterialDialog? = null
+    private var errorDialog: MaterialDialog? = null
 
     private var insertionData: InsertionData = InsertionData()
 
 
     override fun prevScreen() = with(publishViewPager) {
-        Log.d(TAG, "Prev; Insertion data: $insertionData")
+        Log.d(TAG, "Prev;\n>Insertion data: $insertionData")
         currentItem -= 1
     }
 
     override fun nextScreen() = with(publishViewPager) {
-        Log.d(TAG, "Next; Insertion data: $insertionData")
+        Log.d(TAG, "Next;\n>Insertion data: $insertionData")
         currentItem += 1
     }
 
@@ -66,7 +71,24 @@ class InsertionPublishActivity : AppCompatActivity(), BasePublishFragment.Publis
     }
 
     override fun setIsbn(isbn: String) {
+        if (insertionData.bookData.isbn != "") return
+
         insertionData.bookData.isbn = isbn
+
+        showProgressDialog()
+        doAsync {
+            val bData = presenter.getBookDataByIsbn(isbn)
+            uiThread {
+                progressDialog?.dismiss()
+                if (bData != null) {
+                    insertionData.bookData = bData
+                    Log.d(TAG, "Book data found and set")
+                    nextScreen()
+                } else {
+                    showErrorDialog()
+                }
+            }
+        }
     }
 
     override fun isIsbnSet(): Boolean = insertionData.bookData.isbn != ""
@@ -162,10 +184,35 @@ class InsertionPublishActivity : AppCompatActivity(), BasePublishFragment.Publis
         alertQuitDialog?.show()
     }
 
+    private fun showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = MaterialDialog.Builder(this)
+                    .content(R.string.progress_get_book_info)
+                    .progress(true, 0) // Indeterminate progress dialog
+                    .cancelable(false) // Clicking outside the dialog does not close it
+                    .build()
+        }
+        progressDialog?.show()
+    }
+
+    private fun showErrorDialog() {
+        if (errorDialog == null) {
+            errorDialog = MaterialDialog.Builder(this)
+                    .title(R.string.dialog_title_book_info_not_found)
+                    .content(R.string.dialog_content_book_info_not_found)
+                    .positiveText(R.string.str_continue)
+                    .onPositive { materialDialog, dialogAction ->  nextScreen() }
+                    .build()
+        }
+        errorDialog?.show()
+    }
+
     /**
      * Dismiss eventual open dialogs to prevent leaked windows.
      */
     private fun dismissDialogs() {
+        Log.d(TAG, "Dismissing dialogs")
+        val dialogs = listOf(alertQuitDialog, progressDialog, errorDialog)
         dialogs.forEach { it?.dismiss() }
     }
 
