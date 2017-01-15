e6240d6b267e240287a6bad4686ecf6816bb5e8e
Edoardo Scibona <velut@users.noreply.github.com>
2017-01-15 19:06:43 +0100

    Simplify ServerDataSender by using ACLs when saving documents. Implement sendMessageDocument()

--- app/src/main/java/com/nibokapp/nibok/server/send/ServerDataSender.kt
+++ app/src/main/java/com/nibokapp/nibok/server/send/ServerDataSender.kt
@@ -1,10 +1,10 @@
 package com.nibokapp.nibok.server.send
 
 import android.util.Log
+import com.baasbox.android.BaasACL
 import com.baasbox.android.BaasDocument
 import com.baasbox.android.Grant
 import com.baasbox.android.Role
-import com.nibokapp.nibok.extension.onSuccess
 import com.nibokapp.nibok.server.send.common.ServerDataSenderInterface
 
 class ServerDataSender : ServerDataSenderInterface {
@@ -22,80 +22,61 @@ class ServerDataSender : ServerDataSenderInterface {
      * BOOK DATA
      */
 
-    override fun sendBookDocument(document: BaasDocument): Boolean {
+    override fun sendBookDocument(document: BaasDocument): Pair<Boolean, String?> {
         Log.d(TAG, "Sending book to server")
-        return document.sendForPublicReading()
+        val sent = document.sendForPublicReading()
+        return Pair(sent, document.getIdOrNull(sent))
     }
 
     /*
      * INSERTIONS
      */
 
-    override fun sendInsertionDocument(document: BaasDocument): Boolean {
+    override fun sendInsertionDocument(document: BaasDocument): Pair<Boolean, String?> {
         Log.d(TAG, "Sending insertion to server")
-        return document.sendForPublicReading()
+        val sent = document.sendForPublicReading()
+        return Pair(sent, document.getIdOrNull(sent))
     }
 
     /*
      * CONVERSATIONS
      */
 
-    override fun sendConversationDocument(document: BaasDocument, partnerId: String): Boolean {
+    override fun sendConversationDocument(document: BaasDocument, partnerId: String): Pair<Boolean, String?> {
         Log.d(TAG, "Sending conversation to server")
-        return document.sendAndGrantOneAccess(Grant.UPDATE, partnerId)
+        // ACL allowing the conversation's partner to read and update the conversation document
+        val acl = BaasACL.grantUser(partnerId, Grant.READ, Grant.UPDATE)
+        val sent = document.saveSync(acl).isSuccess
+        return Pair(sent, document.getIdOrNull(sent))
     }
 
     /*
      * MESSAGES
      */
 
+    override fun sendMessageDocument(document: BaasDocument, recipientId: String): Pair<Boolean, String?> {
+        Log.d(TAG, "Sending message to server")
+        // ACL allowing the message's recipient to read the message document
+        val acl = BaasACL.grantUser(recipientId, Grant.READ)
+        val sent = document.saveSync(acl).isSuccess
+        return Pair(sent, document.getIdOrNull(sent))
+    }
+
     /*
      * EXTENSIONS
      */
 
     /**
-     * Send this BaasDocument to the server and once saved successfully execute the given function.
-     */
-    private fun BaasDocument.sendAndDo(func: (BaasDocument) -> Boolean): Boolean {
-        var sent = false
-        this.saveSync().onSuccess { sent = func(it) }
-        return sent
-    }
-
-    /**
-     * Send this BaasDocument to the server and grant the given access level to the given user.
-     *
-     * @param accessLevel the access level granted to the user for the document
-     * @param accessingUserId the id of the user accessing the document
-     *
-     * @return true if the BaasDocument was correctly sent and setup
-     */
-    private fun BaasDocument.sendAndGrantOneAccess(accessLevel: Grant, accessingUserId: String): Boolean {
-        return this.sendAndDo {
-            Log.d(TAG, "Document saved on server, granting access")
-            it.grantSync(accessLevel, accessingUserId).isSuccess
-        }
-    }
-
-    /**
-     * Send this BaasDocument to the server and grant the given access level to the specified user roles.
-     *
-     * @param accessLevel the access level granted to the user for the document
-     * @param accessedBy the category of users accessing the document
-     *
-     * @return true if the BaasDocument was correctly sent and setup
-     */
-    private fun BaasDocument.sendAndGrantAllAccess(accessLevel: Grant, accessedBy: String): Boolean {
-        return this.sendAndDo {
-            Log.d(TAG, "Document saved on server, granting access")
-            it.grantAllSync(accessLevel, accessedBy).isSuccess
-        }
-    }
-
-    /**
      * Send this BaasDocument and let it be readable by everyone (including anonymous users).
      */
     private fun BaasDocument.sendForPublicReading(): Boolean {
-        return this.sendAndGrantAllAccess(Grant.READ, Role.ANONYMOUS)
+        // Access Control List allowing anonymous users to read this document
+        val acl = BaasACL.grantRole(Role.ANONYMOUS, Grant.READ)
+        return this.saveSync(acl).isSuccess
     }
+
+    /**
+     * Get the id of this document if it was saved, null otherwise.
+     */
+    private fun BaasDocument.getIdOrNull(sent: Boolean): String? = if (sent) this.id else null
 }
