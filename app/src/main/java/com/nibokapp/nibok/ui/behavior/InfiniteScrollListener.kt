47da2bdeaaa5faa0f8fb4ac1ca3e12da0d2d5748
Edoardo Scibona <velut@users.noreply.github.com>
2017-01-24 16:54:32 +0100

    Update InfiniteScrollListener to work by buffer size

--- app/src/main/java/com/nibokapp/nibok/ui/behavior/InfiniteScrollListener.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/behavior/InfiniteScrollListener.kt
@@ -13,20 +13,21 @@ import android.support.v7.widget.RecyclerView
  * @param layoutManager the layout manager
  * @param loadOnScrollDown true if the listener should listen to scroll down events,
  *                         false for scroll up events. Default is true
+ * @param bufferSize the minimum size of the buffer,
+ *                   if the current buffer contains less items than bufferSize new items are loaded.
+ *                   Default buffer size is 10
  * @param loadFunc the function to be called to load more items
  */
 class InfiniteScrollListener(val layoutManager: LinearLayoutManager,
                              val loadOnScrollDown: Boolean = true,
+                             val bufferSize: Int = 10,
                              val loadFunc: () -> Unit) :
         RecyclerView.OnScrollListener() {
-
-    private val VISIBLE_THRESHOLD = 3
 
     private var loading = true
     private var previousTotal = 0
     private var totalItemCount = 0
-    private var visibleItemCount = 0
-    private var firstVisibleItemPosition = 0
+    private var newestVisibleItemPosition = 0
 
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -34,11 +35,18 @@ class InfiniteScrollListener(val layoutManager: LinearLayoutManager,
 
         val hasScrolled = if (loadOnScrollDown) dy > 0 else dy < 0
 
-        // The view was scrolled
         if (hasScrolled) {
+            // Total items contained in the adapter bound to the RecyclerView
             totalItemCount = layoutManager.itemCount
-            visibleItemCount = layoutManager.childCount
-            firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+            // The newest visible item is the one that just entered the view while scrolling
+            // Thus it is the last visible item (bottom) when scrolling down
+            // and the first visible item (top) when scrolling up
+            newestVisibleItemPosition = if (loadOnScrollDown) {
+                layoutManager.findLastVisibleItemPosition()
+            } else {
+                layoutManager.findFirstVisibleItemPosition()
+            }
 
             // There are enough items, stop loading
             if (loading && (totalItemCount > previousTotal)) {
@@ -54,7 +62,12 @@ class InfiniteScrollListener(val layoutManager: LinearLayoutManager,
         }
     }
 
-    private fun isThresholdReached() =
-            (totalItemCount - visibleItemCount) <= (firstVisibleItemPosition + VISIBLE_THRESHOLD)
-
+    private fun isThresholdReached(): Boolean {
+        val scrollBufferItemCount = if (loadOnScrollDown) {
+            totalItemCount - (newestVisibleItemPosition + 1)
+        } else {
+            newestVisibleItemPosition
+        }
+        return scrollBufferItemCount <= bufferSize
+    }
 }
