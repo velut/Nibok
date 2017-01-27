33e2bec3f96b23c3f0d20fbcbd4b403c472d86ab
Edoardo Scibona <velut@users.noreply.github.com>
2017-01-27 18:41:59 +0100

    Add support for creating image files in the cache directory

--- app/src/main/java/com/nibokapp/nibok/extension/FileExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/FileExtensions.kt
@@ -20,26 +20,30 @@ val TAG = "FileExtensions"
  * Try to create an unique image file in which to store a picture.
  *
  * @param context the context used to retrieve the external file directory
+ * @param inCacheDir true if the file has to be created in the cache directory. Default is false
  *
  * @return a File if the file was created successfully,
  * null if no file could be created
  */
-fun createImageFile(context: Context): File? {
+fun createImageFile(context: Context, inCacheDir: Boolean = false): File? {
     @SuppressLint("SimpleDateFormat")
     val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
     val imageFileName = "JPEG_$timestamp"
     val extension = ".jpg"
 
-    // Get the external public files directory
-    val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    val storageDir = if (inCacheDir) {
+        context.cacheDir
+    } else {
+        // Get the external public files directory
+        context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    }
 
-    val imageFile: File? =
-            try {
-                File.createTempFile(imageFileName, extension, storageDir)
-            } catch (e: IOException) {
-                Log.d(TAG, "Could not create image file\nException: $e")
-                null
-            }
+    val imageFile: File? = try {
+        File.createTempFile(imageFileName, extension, storageDir)
+    } catch (e: IOException) {
+        Log.d(TAG, "Could not create image file\nException: $e")
+        null
+    }
 
     return imageFile
 }
