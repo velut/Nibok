d95033b2109179dbecfc44027df12a71dc1f9e53
Edoardo Scibona <velut@users.noreply.github.com>
2017-01-28 20:09:55 +0100

    Remove compressImage(), add Uri.toInputStream() and InputStream.toFile()

--- app/src/main/java/com/nibokapp/nibok/extension/FileExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/FileExtensions.kt
@@ -5,11 +5,7 @@ import android.content.Context
 import android.net.Uri
 import android.os.Environment
 import android.util.Log
-import com.nibokapp.nibok.ui.App
-import id.zelory.compressor.Compressor
-import id.zelory.compressor.FileUtil
-import java.io.File
-import java.io.IOException
+import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -52,31 +48,49 @@ fun createImageFile(context: Context, inCacheDir: Boolean = false): File? {
     return imageFile
 }
 
-/**
- * Compress the image located at the given file Uri.
- *
- * @param fileUri the uri of the image file
- * @param context the context. Defaults to the App context
- *
- * @return a File with the compressed image or null if the image could not be compressed
- *
- */
-fun compressImage(fileUri: Uri, context: Context = App.instance): File? {
-    val rawImageFile = try {
-        FileUtil.from(context, fileUri) // Use FileUtil from Compressor library
-    } catch (e: IOException) {
-        Log.e(TAG, "Could not get file from uri: $fileUri")
+fun Uri.toInputStream(context: Context): InputStream? {
+    return try {
+        context.contentResolver.openInputStream(this)
+    } catch (e: FileNotFoundException) {
+        Log.e(TAG, "Could not find file associated to: $this")
         e.printStackTrace()
-        return null
+        null
     }
-    Log.d(TAG, "Compressing image")
-    val compressedImageFile = try {
-        Compressor.getDefault(context).compressToFile(rawImageFile)
+}
+
+fun InputStream.toFile(outFilePath: String): File? {
+    val inputStream = this as? FileInputStream ?: return null
+    var outputStream: FileOutputStream? = null
+    try {
+        val tmpFile = File(outFilePath)
+        outputStream = FileOutputStream(tmpFile)
+
+        val bytes = ByteArray(1024)
+
+        while (true) {
+            val read = inputStream.read(bytes)
+            if (read == -1) break
+            outputStream.write(bytes, 0, read)
+        }
+        Log.d(TAG, "Copied input stream to file")
+        return tmpFile
     } catch (e: Exception) {
-        Log.e(TAG, "Could not compress image")
+        Log.e(TAG, "Error while copying to file")
         e.printStackTrace()
         return null
+    } finally {
+        try {
+            inputStream.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error while closing input stream")
+            e.printStackTrace()
+        }
+
+        try {
+            outputStream?.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error while closing output stream")
+            e.printStackTrace()
+        }
     }
-    Log.d(TAG, "Compressed image successfully")
-    return compressedImageFile
 }
