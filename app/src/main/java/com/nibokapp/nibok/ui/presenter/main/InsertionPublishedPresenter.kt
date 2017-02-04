2a2cd22d2b4df7e54e5fbbe7a5f19c4b3d5375c7
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-04 20:16:49 +0100

    Add InsertionPublishedPresenter

---
+++ app/src/main/java/com/nibokapp/nibok/ui/presenter/main/InsertionPublishedPresenter.kt
@@ -0,0 +1,38 @@
+package com.nibokapp.nibok.ui.presenter.main
+
+import android.util.Log
+import com.nibokapp.nibok.domain.command.bookinsertion.published.RequestCachedPublishedBookInsertionCommand
+import com.nibokapp.nibok.domain.command.bookinsertion.published.RequestOlderPublishedBookInsertionCommand
+import com.nibokapp.nibok.domain.command.bookinsertion.published.RequestPublishedBookInsertionCommand
+import com.nibokapp.nibok.domain.command.bookinsertion.published.RequestPublishedBookInsertionFromQueryCommand
+import com.nibokapp.nibok.domain.model.BookInsertionModel
+
+/**
+ * Presenter used for the user published insertions.
+ */
+class InsertionPublishedPresenter : MainActivityPresenter<BookInsertionModel> {
+
+    companion object {
+        private val TAG = InsertionPublishedPresenter::class.java.simpleName
+    }
+
+    override fun getData(): List<BookInsertionModel> {
+        Log.d(TAG, "Requesting insertion published data")
+        return RequestPublishedBookInsertionCommand().execute()
+    }
+
+    override fun getCachedData(): List<BookInsertionModel> {
+        Log.d(TAG, "Requesting cached insertion published data")
+        return RequestCachedPublishedBookInsertionCommand().execute()
+    }
+
+    override fun getDataOlderThanItem(item: BookInsertionModel): List<BookInsertionModel> {
+        Log.d(TAG, "Requesting older insertion published data")
+        return RequestOlderPublishedBookInsertionCommand(item).execute()
+    }
+
+    override fun getQueryData(query: String): List<BookInsertionModel> {
+        Log.d(TAG, "Requesting insertion published data by query")
+        return RequestPublishedBookInsertionFromQueryCommand(query).execute()
+    }
+}
