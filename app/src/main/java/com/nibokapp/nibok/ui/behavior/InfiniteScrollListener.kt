703fd34e7f39f87b10642b3d7ca8a288f7f694b9
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-05 18:46:29 +0100

    Add optional logging

--- app/src/main/java/com/nibokapp/nibok/ui/behavior/InfiniteScrollListener.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/behavior/InfiniteScrollListener.kt
@@ -2,6 +2,7 @@ package com.nibokapp.nibok.ui.behavior
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 
 /**
  * Scroll listener that enables infinite scrolling.
@@ -23,6 +24,12 @@ class InfiniteScrollListener(val layoutManager: LinearLayoutManager,
                              val bufferSize: Int = 10,
                              val loadFunc: () -> Unit) :
         RecyclerView.OnScrollListener() {
+
+    companion object {
+        private val TAG: String = InfiniteScrollListener::class.java.simpleName
+
+        private val VERBOSE: Boolean = false
+    }
 
     private var loading = true
     private var previousTotal = 0
@@ -48,14 +55,22 @@ class InfiniteScrollListener(val layoutManager: LinearLayoutManager,
                 layoutManager.findFirstVisibleItemPosition()
             }
 
-            // There are enough items, stop loading
+            if (VERBOSE) {
+                Log.d(TAG, "User has scrolled")
+                Log.d(TAG, "Total item count: $totalItemCount")
+                Log.d(TAG, "Previous item count: $previousTotal")
+                Log.d(TAG, "Newest visible item position: $newestVisibleItemPosition")
+                Log.d(TAG, "Loading: $loading")
+            }
+
             if (loading && (totalItemCount > previousTotal)) {
+                if (VERBOSE) Log.d(TAG, "Enough items, stop loading")
                 loading = false
                 previousTotal = totalItemCount
             }
 
-            // Almost reached the end, load more items
             if (!loading && isThresholdReached()) {
+                if (VERBOSE) Log.d(TAG, "Loading more items")
                 loadFunc()
                 loading = true
             }
@@ -72,6 +87,7 @@ class InfiniteScrollListener(val layoutManager: LinearLayoutManager,
     }
 
     fun reset() {
+        if (VERBOSE) Log.d(TAG, "Resetting $TAG")
         loading = true
         previousTotal = 0
         totalItemCount = 0
