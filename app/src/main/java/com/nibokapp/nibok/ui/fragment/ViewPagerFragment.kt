e1c7a10460ce0b054aed2aaa1abb883f53d35317
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-07 15:43:16 +0100

    Add onUnselected. Update docs

--- app/src/main/java/com/nibokapp/nibok/ui/fragment/ViewPagerFragment.kt
+++ app/src/main/java/com/nibokapp/nibok/ui/fragment/ViewPagerFragment.kt
@@ -1,13 +1,20 @@
 package com.nibokapp.nibok.ui.fragment
 
 /**
- * Interface for fragments hosted in a ViewPager
+ * Interface for fragments hosted in a ViewPager.
+ *
+ * Used to alert fragments of when they become visible or become hidden in the viewpager.
  */
 interface ViewPagerFragment {
 
     /**
-     * Called when a fragment was selected in the viewpager and is the currently visible one.
+     * Called when a fragment is selected in the viewpager becoming the currently visible one.
      */
     fun onSelected()
+
+    /**
+     * Called when a fragment previously selected in the viewpager is no longer the currently visible one.
+     */
+    fun onUnselected()
 
 }
