e91364dfa990663c5e83b91ac67d74b3cea112eb
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-10 19:33:07 +0100

    Update createLocalUser() and refactor

--- app/src/main/java/com/nibokapp/nibok/data/repository/UserRepository.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/UserRepository.kt
@@ -1,13 +1,12 @@
 package com.nibokapp.nibok.data.repository
 
 import android.util.Log
-import com.nibokapp.nibok.data.db.Conversation
-import com.nibokapp.nibok.data.db.Insertion
 import com.nibokapp.nibok.data.db.User
 import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
 import com.nibokapp.nibok.extension.executeRealmTransaction
 import com.nibokapp.nibok.extension.getLocalUser
 import com.nibokapp.nibok.extension.queryOneRealm
+import com.nibokapp.nibok.extension.toRealmStringList
 
 /**
  * Repository singleton for the local user.
@@ -16,50 +15,41 @@ object UserRepository : UserRepositoryInterface {
 
     const private val TAG = "UserRepository"
 
-    override fun createLocalUser(userId: String,
-                                 savedInsertions: List<Insertion>,
-                                 publishedInsertions: List<Insertion>,
-                                 conversations: List<Conversation>) {
-        while (!localUserExists()) {
-            Log.d(TAG, "Local user does not exist, trying to create one")
-            executeRealmTransaction {
-                // Insert user in the db
-                val newUser = it.createObject(User::class.java, userId)
-                newUser.apply {
-                    this.savedInsertions.addAll(savedInsertions)
-                    this.publishedInsertions.addAll(publishedInsertions)
-                    this.conversations.addAll(conversations)
-                }
-                Log.d(TAG, "Created new user: ${newUser.username}")
-            }
+    override fun createLocalUser(userId: String, savedInsertionsIds: List<String>): Boolean {
+        if (localUserExists()) return true
+        Log.d(TAG, "Local user does not exist, trying to create one")
+        executeRealmTransaction {
+            // Insert user in the db
+            val newUser = it.createObject(User::class.java, userId)
+            newUser.savedInsertionsIds.addAll(savedInsertionsIds.toRealmStringList())
+            Log.d(TAG, "Created new user: ${newUser.username}")
         }
-        Log.d(TAG, "Local user created")
+        return localUserExists()
     }
 
     override fun removeLocalUser(): Boolean {
-
         if (!localUserExists()) {
             Log.d(TAG, "Local user does not exist. Cannot remove it.")
             return false
         }
 
         Log.d(TAG, "Removing local user")
-
         executeRealmTransaction {
             val user = it.getLocalUser()
             user?.deleteFromRealm()
         }
 
         return localUserExists()
-
     }
 
-    override fun getLocalUserId(): String = getLocalUser()?.username ?:
-            throw IllegalStateException(
-                    "Local user does not exist. Call createLocalUser() before retrieving data")
+    override fun getLocalUserId(): String {
+        val userId = getLocalUser()?.username
+        return userId ?: throw IllegalStateException("Local user does not exist. Call createLocalUser() before retrieving data")
+    }
 
-    override fun getLocalUser(): User? =
-            queryOneRealm { it.where(User::class.java).findFirst() }
+    override fun getLocalUser(): User? {
+        return queryOneRealm { it.where(User::class.java).findFirst() }
+    }
 
     override fun localUserExists(): Boolean = getLocalUser() != null
 }
