f94bb3ae95d716c532a4397a6129fb3d94f388ac
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-10 19:46:11 +0100

    Update methods implementation; add utilities and extensions

--- app/src/main/java/com/nibokapp/nibok/data/repository/db/LocalBookInsertionRepository.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/db/LocalBookInsertionRepository.kt
@@ -9,15 +9,14 @@ import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
 import com.nibokapp.nibok.data.repository.db.common.LocalStorage
 import com.nibokapp.nibok.extension.*
 import io.realm.Case
+import io.realm.Realm
+import io.realm.RealmQuery
 import org.jetbrains.anko.doAsync
-import java.util.*
 
 /**
  * Local repository for book insertions.
  */
-object LocalBookInsertionRepository :
-        BookInsertionRepositoryInterface,
-        LocalStorage<Insertion> {
+object LocalBookInsertionRepository : BookInsertionRepositoryInterface, LocalStorage<Insertion> {
 
     const private val TAG = "LocalBookInsertionRepo"
 
@@ -28,178 +27,288 @@ object LocalBookInsertionRepository :
     private var publishedCache: List<Insertion> = emptyList()
 
     /*
-     * COMMON FUNCTIONS
+     * Common functions
      */
 
-    override fun getInsertionById(insertionId: String): Insertion? = queryOneRealm {
-        it.where(Insertion::class.java)
-                .equalTo("id", insertionId)
-                .findFirst()
+    override fun getInsertionById(insertionId: String): Insertion? {
+        val insertion = queryOneRealm {
+            it.whereInsertion()
+                    .idEqualTo(insertionId)
+                    .findFirst()
+        }
+        Log.d(TAG, "For id: $insertionId found: $insertion")
+        return insertion
     }
 
-    override fun getBookByISBN(isbn: String): Book? = queryOneRealm {
-        it.where(Book::class.java)
-                .equalTo("isbn", isbn)
-                .findFirst()
+    override fun getBookByISBN(isbn: String): Book? {
+        val book = queryOneRealm {
+            it.where(Book::class.java)
+                    .equalTo("isbn", isbn)
+                    .findFirst()
+        }
+        Log.d(TAG, "For ISBN: $isbn found book: $book")
+        return book
     }
 
     override fun getInsertionListFromQuery(query: String): List<Insertion> {
-
-        val trimmedQuery = query.trim()
-
-        if (trimmedQuery.isEmpty()) return emptyList()
-
-        val results = queryManyRealm {
-            it.where(Insertion::class.java)
-                    .contains("book.title", trimmedQuery, Case.INSENSITIVE)
-                    .or()
-                    .contains("book.authors.value", trimmedQuery, Case.INSENSITIVE)
-                    .or()
-                    .contains("book.publisher", trimmedQuery, Case.INSENSITIVE)
-                    .or()
-                    .contains("book.isbn", trimmedQuery, Case.INSENSITIVE)
-                    .findAll()
-        }
-        Log.d(TAG, "Book insertions corresponding to query '$query' = ${results.size}")
-        return results
-    }
-
-    override fun getInsertionListAfterDate(date: Date): List<Insertion> {
-        val results = queryManyRealm {
-            it.where(Insertion::class.java)
-                    .greaterThanOrEqualTo("date", date)
-                    .findAll()}
-        Log.d(TAG, "Found ${results.size} insertions after $date")
-        return results
-    }
-
-    override fun getInsertionListBeforeDate(date: Date): List<Insertion> {
-        val results = queryManyRealm {
-            it.where(Insertion::class.java)
-                    .lessThanOrEqualTo("date", date)
-                    .findAll()}
-        Log.d(TAG, "Found ${results.size} insertions before $date")
-        return results
+        val insertions = getInsertionListFromQueryWithFilter(query)
+        Log.d(TAG, "Insertions corresponding to query '$query': ${insertions.size}")
+        return insertions
     }
 
     /*
-     * FEED BOOK INSERTIONS
+     * Feed insertions
      */
 
     override fun getFeedInsertionList(cached: Boolean): List<Insertion> {
         if (cached) return feedCache
-        feedCache = queryManyRealm { it.where(Insertion::class.java).findAll()}
-                .excludeUserOwnInsertions()
+        feedCache = getInsertionList(excludeByLocalUser = true)
         Log.d(TAG, "Found ${feedCache.size} feed insertions")
         return feedCache
     }
 
-    override fun getFeedInsertionListFromQuery(query: String): List<Insertion>  =
-            getInsertionListFromQuery(query).excludeUserOwnInsertions()
+    override fun getFeedInsertionListFromQuery(query: String): List<Insertion> {
+        val insertions = getInsertionListFromQueryWithFilter(query, excludeByLocalUser = true)
+        Log.d(TAG, "Feed insertions corresponding to query '$query': ${insertions.size}")
+        return insertions
+    }
 
-    override fun getFeedInsertionListAfterDate(date: Date): List<Insertion> =
-            getInsertionListAfterDate(date).excludeUserOwnInsertions()
-
-    override fun getFeedInsertionListBeforeDate(date: Date): List<Insertion> =
-            getInsertionListBeforeDate(date).excludeUserOwnInsertions()
+    override fun getFeedInsertionListOlderThanInsertion(insertionId: String): List<Insertion> {
+        val olderInsertions = getOlderInsertionList(insertionId, excludeByLocalUser = true)
+        Log.d(TAG, "Found ${olderInsertions.size} feed insertions older than: $insertionId")
+        return olderInsertions
+    }
 
     /*
-     * SAVED BOOK INSERTIONS
+     * Saved insertions
      */
 
     override fun getSavedInsertionList(cached: Boolean): List<Insertion> {
         if (cached) return savedCache
-        savedCache = userRepository.getLocalUser()?.savedInsertions?.toNormalList() ?: emptyList()
+        savedCache = getInsertionList(includeOnlyIfSaved = true)
+        Log.d(TAG, "Found ${savedCache.size} saved insertions")
         return savedCache
     }
 
-    override fun getSavedInsertionListFromQuery(query: String): List<Insertion> =
-            getInsertionListFromQuery(query).includeOnlySavedInsertions()
+    override fun getSavedInsertionListFromQuery(query: String): List<Insertion> {
+        val insertions = getInsertionListFromQueryWithFilter(query, includeOnlyIfSaved = true)
+        Log.d(TAG, "Saved insertions corresponding to query '$query': ${insertions.size}")
+        return insertions
+    }
 
-    override fun getSavedInsertionLisAfterDate(date: Date): List<Insertion> =
-            getInsertionListAfterDate(date).includeOnlySavedInsertions()
-
-    override fun getSavedInsertionListBeforeDate(date: Date): List<Insertion> =
-            getInsertionListBeforeDate(date).includeOnlySavedInsertions()
+    override fun getSavedInsertionListOlderThanInsertion(insertionId: String): List<Insertion> {
+        val olderInsertions = getOlderInsertionList(insertionId, includeOnlyIfSaved = true)
+        Log.d(TAG, "Found ${olderInsertions.size} saved insertions older than: $insertionId")
+        return olderInsertions
+    }
 
     /*
-     * PUBLISHED BOOK INSERTIONS
+     * Published insertions
      */
 
     override fun getPublishedInsertionList(cached: Boolean): List<Insertion> {
         if (cached) return publishedCache
-        publishedCache =
-                userRepository.getLocalUser()?.publishedInsertions?.toNormalList() ?: emptyList()
+        publishedCache = getInsertionList(includeOnlyByLocalUser = true)
+        Log.d(TAG, "Found ${publishedCache.size} published insertions")
         return publishedCache
     }
 
-    override fun getPublishedInsertionListFromQuery(query: String): List<Insertion> =
-            getInsertionListFromQuery(query).includeOnlyUserOwnInsertions()
+    override fun getPublishedInsertionListFromQuery(query: String): List<Insertion> {
+        val insertions = getInsertionListFromQueryWithFilter(query, includeOnlyByLocalUser = true)
+        Log.d(TAG, "Published insertions corresponding to query '$query': ${insertions.size}")
+        return insertions
+    }
 
-    override fun getPublishedInsertionListAfterDate(date: Date): List<Insertion> =
-            getInsertionListAfterDate(date).includeOnlyUserOwnInsertions()
+    override fun getPublishedInsertionListOlderThanInsertion(insertionId: String): List<Insertion> {
+        val olderInsertions = getOlderInsertionList(insertionId, includeOnlyByLocalUser = true)
+        Log.d(TAG, "Found ${olderInsertions.size} published insertions older than: $insertionId")
+        return olderInsertions
+    }
 
-    override fun getPublishedInsertionListBeforeDate(date: Date): List<Insertion> =
-            getInsertionListBeforeDate(date).includeOnlyUserOwnInsertions()
-
-    /*
-     * BOOK INSERTION SAVE STATUS
-     */
-
-    override fun isBookInsertionSaved(insertionId: String): Boolean =
-            insertionId in getSavedInsertionList().map { it.id }
-
-    override fun toggleInsertionSaveStatus(insertionId: String): Boolean {
-
-        if (!userRepository.localUserExists())
-            throw IllegalStateException("Local user does not exist. Cannot save insertion")
-
-        var saved = false
-
-        withRealm {
-            val insertion = it.getBookInsertionById(insertionId)
-            val user = it.getLocalUser()
-            val savedInsertions = user!!.savedInsertions
-
-            saved = insertion in savedInsertions
-
-            it.executeTransaction {
-                if (!saved) {
-                    savedInsertions.add(0, insertion)
-                } else {
-                    savedInsertions.remove(insertion)
-                }
-            }
-            saved = insertion in savedInsertions
-            Log.d(TAG, "After toggle: Save status: $saved, saved size: ${savedInsertions.size}")
+    override fun deletePublishedInsertion(insertionId: String): Boolean {
+        executeRealmTransaction {
+            val insertion = it.whereInsertion().idEqualTo(insertionId).findAll()
+            insertion.deleteAllFromRealm()
         }
-        return saved
+        return true
     }
 
     /*
-     * BOOK INSERTION PUBLISHING
+     * Insertion's save status
      */
 
-    override fun publishInsertion(insertion: Insertion): Boolean =
-            // TODO
-            throw UnsupportedOperationException()
+    override fun isBookInsertionSaved(insertionId: String): Boolean {
+        val isSaved = insertionId in getSavedInsertionList().map { it.id }
+        Log.d(TAG, "Insertion: $insertionId is saved: $isSaved")
+        return isSaved
+    }
+
+    override fun toggleInsertionSaveStatus(insertionId: String): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    fun setInsertionSaveStatus(insertionId: String, isSaved: Boolean) {
+        if (!localUserExists()) return
+        val rInsertionId = insertionId.toRealmString()
+        executeRealmTransaction {
+            it.getLocalUser()?.let {
+                if (isSaved) {
+                    it.savedInsertionsIds.add(rInsertionId)
+                } else {
+                    it.savedInsertionsIds.remove(rInsertionId)
+                }
+            }
+        }
+    }
 
     /*
-     * LOCAL STORAGE
+     * Insertion publishing
      */
+
+    override fun publishInsertion(insertion: Insertion): Boolean {
+        throw UnsupportedOperationException()
+    }
+
+    /*
+     * Local storage
+     */
+
     override fun storeItems(items: List<Insertion>) {
 
         if (items.isEmpty()) return
         doAsync {
             Log.d(TAG, "Storing insertions")
-            items.forEach { storeInsertion(it) }
+            items.forEach { storeItem(it) }
         }
     }
 
-    private fun storeInsertion(insertion: Insertion) {
-        Log.d(TAG, "Storing insertion: ${insertion.id}")
+    override fun storeItem(item: Insertion) {
+        Log.d(TAG, "Storing insertion: ${item.id}")
         executeRealmTransaction {
-            it.copyToRealmOrUpdate(insertion)
+            it.copyToRealmOrUpdate(item)
         }
     }
+
+    /*
+     * Utilities
+     */
+
+    private fun localUserExists(): Boolean {
+        return userRepository.localUserExists()
+    }
+
+    private fun getLocalUserId(): String {
+        return userRepository.getLocalUserId()
+    }
+
+    /*
+     * General querying with filtering
+     */
+
+    private fun getInsertionList(excludeByLocalUser: Boolean = false,
+                                 includeOnlyIfSaved: Boolean = false,
+                                 includeOnlyByLocalUser: Boolean = false): List<Insertion> {
+        return queryManyRealm {
+            it.whereInsertion()
+                    .applyFiltering(excludeByLocalUser, includeOnlyIfSaved, includeOnlyByLocalUser)
+                    .findAll()
+        }
+    }
+
+    private fun getInsertionListFromQueryWithFilter(query: String,
+                                                    excludeByLocalUser: Boolean = false,
+                                                    includeOnlyIfSaved: Boolean = false,
+                                                    includeOnlyByLocalUser: Boolean = false): List<Insertion> {
+        val trimmedQuery = query.trim()
+        if (trimmedQuery.isEmpty()) return emptyList()
+
+        return queryManyRealm {
+            it.queryInsertion(query)
+                    .applyFiltering(excludeByLocalUser, includeOnlyIfSaved, includeOnlyByLocalUser)
+                    .findAll()
+        }
+    }
+
+    private fun getOlderInsertionList(insertionId: String,
+                                      excludeByLocalUser: Boolean = false,
+                                      includeOnlyIfSaved: Boolean = false,
+                                      includeOnlyByLocalUser: Boolean = false): List<Insertion> {
+        val currentOldestDate = getInsertionById(insertionId)?.date ?: return emptyList()
+        return queryManyRealm {
+            it.whereInsertion()
+                    .olderThan(insertionId, currentOldestDate)
+                    .applyFiltering(excludeByLocalUser, includeOnlyIfSaved, includeOnlyByLocalUser)
+                    .findAll()
+        }
+    }
+
+    /*
+     * Extensions
+     */
+
+    private fun Realm.whereInsertion(): RealmQuery<Insertion> {
+        return this.where(Insertion::class.java)
+    }
+
+    private fun Realm.queryInsertion(query: String): RealmQuery<Insertion> {
+        return this.whereInsertion()
+                .contains("book.title", query, Case.INSENSITIVE)
+                .or()
+                .contains("book.authors.value", query, Case.INSENSITIVE)
+                .or()
+                .contains("book.publisher", query, Case.INSENSITIVE)
+                .or()
+                .contains("book.isbn", query, Case.INSENSITIVE)
+    }
+
+    private fun RealmQuery<Insertion>.excludeByLocalUser(userId: String): RealmQuery<Insertion> {
+        return this.notEqualTo("seller.username", userId)
+    }
+
+    private fun RealmQuery<Insertion>.excludeByLocalUserIfPossible(): RealmQuery<Insertion> {
+        return if (localUserExists()) {
+            this.excludeByLocalUser(getLocalUserId())
+        } else {
+            this
+        }
+    }
+
+    private fun RealmQuery<Insertion>.whereIdInList(idList: List<String>): RealmQuery<Insertion> {
+        return this.`in`("id", idList.toTypedArray())
+    }
+
+    private fun RealmQuery<Insertion>.includeOnlySavedIfPossible(): RealmQuery<Insertion> {
+        val localUser = userRepository.getLocalUser()
+        val savedInsertionIds = localUser?.savedInsertionsIds?.toStringList() ?: emptyList()
+        return this.whereIdInList(savedInsertionIds)
+    }
+
+    private fun RealmQuery<Insertion>.includeOnlyByLocalUser(userId: String): RealmQuery<Insertion> {
+        return this.equalTo("seller.username", userId)
+    }
+
+    private fun RealmQuery<Insertion>.includeOnlyByLocalUserIfPossible(): RealmQuery<Insertion> {
+        if (!localUserExists()) return this.whereIdInList(emptyList())
+        val publishedInsertionIds = queryManyRealm {
+            it.whereInsertion()
+                    .includeOnlyByLocalUser(getLocalUserId())
+                    .findAll()
+        }.map { it.id }
+        return this.whereIdInList(publishedInsertionIds)
+    }
+
+    private fun RealmQuery<Insertion>.applyFiltering(excludeByLocalUser: Boolean,
+                                                     includeOnlyIfSaved: Boolean,
+                                                     includeOnlyByLocalUser: Boolean): RealmQuery<Insertion> {
+        return if (excludeByLocalUser) {
+            this.excludeByLocalUserIfPossible()
+        } else if (includeOnlyIfSaved) {
+            this.includeOnlySavedIfPossible()
+        } else if (includeOnlyByLocalUser) {
+            this.includeOnlyByLocalUserIfPossible()
+        } else {
+            this
+        }
+    }
+
 }
