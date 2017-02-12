8810fbf79f0639d6d26407b831201371bb348405
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-12 22:13:39 +0100

    Add conversation and message storing

--- app/src/main/java/com/nibokapp/nibok/data/repository/ConversationRepository.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/ConversationRepository.kt
@@ -6,8 +6,10 @@ import com.nibokapp.nibok.data.db.Message
 import com.nibokapp.nibok.data.repository.common.ConversationRepositoryInterface
 import com.nibokapp.nibok.data.repository.db.LocalConversationRepository
 import com.nibokapp.nibok.data.repository.server.ServerConversationRepository
-import com.nibokapp.nibok.extension.firstListResultOrNull
+import com.nibokapp.nibok.extension.firstListResultOrNullWithStorage
 import com.nibokapp.nibok.extension.firstResultOrNull
+import com.nibokapp.nibok.extension.storeAndReturnResult
+import io.realm.RealmModel
 
 /**
  * Repository for conversations.
@@ -26,68 +28,106 @@ object ConversationRepository : ConversationRepositoryInterface {
     private var conversationCache: List<Conversation> = emptyList()
 
 
-    override fun getConversationById(conversationId: String): Conversation? =
-            SOURCES.firstResultOrNull { it.getConversationById(conversationId) }
+    /*
+     * Conversation
+     */
 
-    override fun getConversationPartnerName(conversationId: String): String? =
-            SOURCES.firstResultOrNull { it.getConversationPartnerName(conversationId) }
+    override fun getConversationById(conversationId: String): Conversation? {
+        return SOURCES.firstResultOrNull { it.getConversationById(conversationId) }
+                .storeAndReturnResult { c, s -> storeInLocalRepo(c, s) }
+    }
 
-    override fun getConversationPreviewText(conversationId: String): String? =
-            SOURCES.firstResultOrNull { it.getConversationPreviewText(conversationId) }
+    override fun getConversationPartnerName(conversationId: String): String? {
+        return SOURCES.firstResultOrNull { it.getConversationPartnerName(conversationId) }.first
+    }
+
+    override fun getConversationPreviewText(conversationId: String): String? {
+        return SOURCES.firstResultOrNull { it.getConversationPreviewText(conversationId) }.first
+    }
 
     override fun getConversationListFromQuery(query: String): List<Conversation> {
-
-        val trimmedQuery = query.trim()
-
-        if (trimmedQuery.isEmpty()) return emptyList()
-
-        val results = SOURCES.firstListResultOrNull { it.getConversationListFromQuery(trimmedQuery) }
-                ?: emptyList()
-
-        // TODO store
-        Log.d(TAG, "Conversations corresponding to query '$trimmedQuery' = ${results.size}")
-        return results
+        val results = SOURCES.firstListResultOrNullWithLocalStorage {
+            it.getConversationListFromQuery(query)
+        } ?: emptyList()
+        val conversations = results.filterIsInstance<Conversation>()
+        Log.d(TAG, "Conversations corresponding to query '$query' = ${conversations.size}")
+        return conversations
     }
 
     override fun getConversationList(cached: Boolean): List<Conversation> {
         if (cached) return conversationCache
-        conversationCache = SOURCES.firstListResultOrNull { it.getConversationList(cached) }
-                ?: emptyList()
-
-        // TODO Store
+        val results = SOURCES.firstListResultOrNullWithLocalStorage {
+            it.getConversationList(cached)
+        } ?: emptyList()
+        conversationCache = results.filterIsInstance<Conversation>()
         Log.d(TAG, "Found ${conversationCache.size} conversations")
         return conversationCache
     }
 
     override fun getConversationListOlderThanConversation(conversationId: String): List<Conversation> {
-        return SOURCES.firstListResultOrNull { it.getConversationListOlderThanConversation(conversationId) } ?: emptyList()
+        val results = SOURCES.firstListResultOrNullWithLocalStorage {
+            it.getConversationListOlderThanConversation(conversationId)
+        } ?: emptyList()
+        return results.filterIsInstance<Conversation>()
     }
 
     override fun startConversation(partnerId: String): String? {
         Log.d(TAG, "Starting conversation with user: $partnerId")
-        return SOURCES.firstResultOrNull { it.startConversation(partnerId) }
+        return SOURCES.firstResultOrNull { it.startConversation(partnerId) }.first
     }
 
     override fun getMessageListForConversation(conversationId: String): List<Message> {
-        val messages = SOURCES.firstListResultOrNull { it.getMessageListForConversation(conversationId) }
-                ?: emptyList()
+        val results = SOURCES.firstListResultOrNullWithLocalStorage {
+            it.getMessageListForConversation(conversationId)
+        } ?: emptyList()
+        val messages = results.filterIsInstance<Message>()
         Log.d(TAG, "Found ${messages.size} messages for conversation: $conversationId")
         return messages
     }
 
     override fun getMessageListBeforeDateOfMessage(messageId: String): List<Message> {
-        val messages = SOURCES.firstListResultOrNull { it.getMessageListBeforeDateOfMessage(messageId) } ?: emptyList()
+        val results = SOURCES.firstListResultOrNullWithLocalStorage {
+            it.getMessageListBeforeDateOfMessage(messageId)
+        } ?: emptyList()
+        val messages = results.filterIsInstance<Message>()
         Log.d(TAG, "Found ${messages.size} messages older than message: $messageId")
         return messages
     }
 
     override fun getMessageListAfterDateOfMessage(messageId: String): List<Message> {
-        val messages = SOURCES.firstListResultOrNull { it.getMessageListAfterDateOfMessage(messageId) } ?: emptyList()
+        val results = SOURCES.firstListResultOrNullWithLocalStorage {
+            it.getMessageListAfterDateOfMessage(messageId)
+        } ?: emptyList()
+        val messages = results.filterIsInstance<Message>()
         Log.d(TAG, "Found ${messages.size} messages newer than message: $messageId")
         return messages
     }
 
     override fun sendMessage(message: Message): String? {
         return serverRepository.sendMessage(message)
+    }
+
+    /*
+     * Utilities
+     */
+
+    private fun storeInLocalRepo(item: RealmModel?, source: ConversationRepositoryInterface?) {
+        if (source == serverRepository && item != null) {
+            localRepository.storeItem(item)
+        }
+    }
+
+    private fun storeInLocalRepo(itemList: List<RealmModel>?, source: ConversationRepositoryInterface?) {
+        if (source == serverRepository && itemList != null) {
+            localRepository.storeItems(itemList)
+        }
+    }
+
+    private inline fun Iterable<ConversationRepositoryInterface>.firstListResultOrNullWithLocalStorage(
+            predicate: (ConversationRepositoryInterface) -> List<RealmModel>?): List<RealmModel>? {
+        return this.firstListResultOrNullWithStorage(
+                { predicate(it) },
+                { resultList, source -> storeInLocalRepo(resultList, source) }
+        )
     }
 }
