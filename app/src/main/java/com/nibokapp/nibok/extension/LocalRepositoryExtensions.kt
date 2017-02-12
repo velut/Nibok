bc457ec8543597fb20aad45ea4fab3fdadf7c188
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-12 22:12:25 +0100

    Add newerThan() and sorting by date extensions

--- app/src/main/java/com/nibokapp/nibok/extension/LocalRepositoryExtensions.kt
+++ app/src/main/java/com/nibokapp/nibok/extension/LocalRepositoryExtensions.kt
@@ -1,9 +1,7 @@
 package com.nibokapp.nibok.extension
 
 import com.nibokapp.nibok.data.db.User
-import io.realm.Realm
-import io.realm.RealmModel
-import io.realm.RealmQuery
+import io.realm.*
 import java.util.*
 
 /**
@@ -39,6 +37,17 @@ fun <T : RealmModel> RealmQuery<T>.dateOlderThan(date: Date): RealmQuery<T> {
 }
 
 /**
+ * Add to a query the condition that the RealmModel's date must be newer than the given one.
+ *
+ * @param date the date used in the query
+ *
+ * @return a RealmQuery
+ */
+fun <T : RealmModel> RealmQuery<T>.dateNewerThan(date: Date): RealmQuery<T> {
+    return this.greaterThanOrEqualTo("date", date)
+}
+
+/**
  * Add to a query the condition that the RealmModel's id must be equal to the given id.
  *
  * @param id the id used in the query
@@ -71,4 +80,31 @@ fun <T : RealmModel> RealmQuery<T>.idNotEqualTo(id: String): RealmQuery<T> {
  */
 fun <T : RealmModel> RealmQuery<T>.olderThan(otherId: String, otherDate: Date): RealmQuery<T> {
     return this.idNotEqualTo(otherId).dateOlderThan(otherDate)
+}
+
+/**
+ * Add to a query the condition that the RealmModel's must be newer
+ * than the one with the given id and date.
+ *
+ * @param otherId the id of the other item
+ * @param otherDate the date of the other item
+ *
+ * @return a RealmQuery
+ */
+fun <T : RealmModel> RealmQuery<T>.newerThan(otherId: String, otherDate: Date): RealmQuery<T> {
+    return this.idNotEqualTo(otherId).dateNewerThan(otherDate)
+}
+
+/**
+ * Find all the results and sort them by descending date (newest to oldest).
+ */
+fun <T : RealmModel> RealmQuery<T>.findAllSortedByDescendingDate(): RealmResults<T> {
+    return this.findAllSorted("date", Sort.DESCENDING)
+}
+
+/**
+ * Find all the results and sort them by ascending date (oldest to newest).
+ */
+fun <T : RealmModel> RealmQuery<T>.findAllSortedByAscendingDate(): RealmResults<T> {
+    return this.findAllSorted("date", Sort.ASCENDING)
 }
