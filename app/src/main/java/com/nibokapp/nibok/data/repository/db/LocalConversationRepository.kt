27590d6035e174fdaab10990a9b662622f965c9a
Edoardo Scibona <velut@users.noreply.github.com>
2017-02-12 22:16:20 +0100

    Update implementation and add storing

--- app/src/main/java/com/nibokapp/nibok/data/repository/db/LocalConversationRepository.kt
+++ app/src/main/java/com/nibokapp/nibok/data/repository/db/LocalConversationRepository.kt
@@ -6,15 +6,18 @@ import com.nibokapp.nibok.data.db.Message
 import com.nibokapp.nibok.data.repository.UserRepository
 import com.nibokapp.nibok.data.repository.common.ConversationRepositoryInterface
 import com.nibokapp.nibok.data.repository.common.UserRepositoryInterface
-import com.nibokapp.nibok.extension.queryManyRealm
-import com.nibokapp.nibok.extension.queryOneRealm
-import com.nibokapp.nibok.extension.toNormalList
+import com.nibokapp.nibok.data.repository.db.common.LocalStorage
+import com.nibokapp.nibok.extension.*
 import io.realm.Case
+import io.realm.Realm
+import io.realm.RealmModel
+import io.realm.RealmQuery
+import org.jetbrains.anko.doAsync
 
 /**
  * Local repository for conversations.
  */
-object LocalConversationRepository : ConversationRepositoryInterface {
+object LocalConversationRepository : ConversationRepositoryInterface, LocalStorage<RealmModel> {
 
     private const val TAG = "LocalConversationRepo"
 
@@ -22,42 +25,44 @@ object LocalConversationRepository : ConversationRepositoryInterface {
 
     private var conversationCache: List<Conversation> = emptyList()
 
+    /*
+     * Conversation
+     */
 
     override fun getConversationById(conversationId: String): Conversation? {
-        if (userRepository.localUserExists()) {
-            return queryOneRealm {
-                it.where(Conversation::class.java)
-                        .equalTo("id", conversationId)
-                        .equalTo("userId", userRepository.getLocalUserId())
-                        .findFirst()
-            }
-        } else {
-            return null
+        if (!localUserExists()) return null
+        val conversation = queryOneRealm {
+            it.whereConversation()
+                    .idEqualTo(conversationId)
+                    .userIdEqualTo(getLocalUserId())
+                    .findFirst()
         }
+        Log.d(TAG, "For id: $conversationId found: $conversation")
+        return conversation
     }
 
-    override fun getConversationPartnerName(conversationId: String): String? =
-            getConversationById(conversationId)?.partner?.username
+    override fun getConversationPartnerName(conversationId: String): String? {
+        return getConversationById(conversationId)?.partner?.username
+    }
 
     override fun getConversationPreviewText(conversationId: String): String? {
-        return null
+        if (!localUserExists()) return null
+        val newestMessage = queryManyRealm {
+            it.whereMessage()
+                    .conversationIdEqualTo(conversationId)
+                    .findAllSortedByDescendingDate()
+        }.getOrNull(0)
+        return newestMessage?.text
     }
 
     override fun getConversationListFromQuery(query: String): List<Conversation> {
-
-        if (!userRepository.localUserExists()) return emptyList()
+        if (!localUserExists()) return emptyList()
 
         val trimmedQuery = query.trim()
-
         if (trimmedQuery.isEmpty()) return emptyList()
 
         val results = queryManyRealm {
-            it.where(Conversation::class.java)
-                    .equalTo("userId", userRepository.getLocalUserId())
-                    .contains("partner.username", trimmedQuery, Case.INSENSITIVE)
-                    .or()
-                    .contains("latestMessage.text", trimmedQuery, Case.INSENSITIVE)
-                    .findAll()
+            it.queryConversation(trimmedQuery).findAllSortedByDescendingDate()
         }
         Log.d(TAG, "Conversations corresponding to query '$trimmedQuery' = ${results.size}")
         return results
@@ -65,43 +70,155 @@ object LocalConversationRepository : ConversationRepositoryInterface {
 
     override fun getConversationList(cached: Boolean): List<Conversation> {
         if (cached) return conversationCache
-        conversationCache =
-                userRepository.getLocalUser()?.conversations?.toNormalList() ?: emptyList()
+        conversationCache = queryManyRealm {
+            it.whereConversation()
+                    .findAllSortedByDescendingDate()
+        }
         Log.d(TAG, "Found ${conversationCache.size} conversations")
         return conversationCache
     }
 
     override fun getConversationListOlderThanConversation(conversationId: String): List<Conversation> {
-        return emptyList()
+        val currentOldestDate = getConversationById(conversationId)?.date ?: return emptyList()
+        return queryManyRealm {
+            it.whereConversation()
+                    .olderThan(conversationId, currentOldestDate)
+                    .findAllSortedByDescendingDate()
+        }
     }
 
     override fun startConversation(partnerId: String): String? {
-        if (!userRepository.localUserExists()) return null
+        if (!localUserExists()) return null
 
         val conversation = queryOneRealm {
-            it.where(Conversation::class.java)
-                    .equalTo("userId", userRepository.getLocalUserId())
-                    .equalTo("partner.username", partnerId)
+            it.whereConversation()
+                    .userIdEqualTo(getLocalUserId())
+                    .partnerIdEqualTo(partnerId)
                     .findFirst()
         }
         return conversation?.id
     }
 
+    /*
+     * Messages
+     */
+
+    private fun getMessageById(messageId: String): Message? {
+        return queryOneRealm {
+            it.whereMessage()
+                    .idEqualTo(messageId)
+                    .findFirst()
+        }
+    }
+
     override fun getMessageListForConversation(conversationId: String): List<Message> {
-//        val conversation = getConversationById(conversationId)
-//        return conversation?.messages?.toNormalList() ?: emptyList()
-        return emptyList()
+        return queryManyRealm {
+            it.whereMessage()
+                    .conversationIdEqualTo(conversationId)
+                    .findAllSortedByAscendingDate()
+        }
     }
 
     override fun getMessageListBeforeDateOfMessage(messageId: String): List<Message> {
-        return emptyList()
+        val currentOldestMessageDate = getMessageById(messageId)?.date
+                ?: return emptyList()
+
+        return queryManyRealm {
+            it.whereMessage()
+                    .olderThan(messageId, currentOldestMessageDate)
+                    .findAllSortedByAscendingDate()
+        }
     }
 
     override fun getMessageListAfterDateOfMessage(messageId: String): List<Message> {
-        return emptyList()
+        val currentNewestMessageDate = getMessageById(messageId)?.date
+                ?: return emptyList()
+
+        return queryManyRealm {
+            it.whereMessage()
+                    .newerThan(messageId, currentNewestMessageDate)
+                    .findAllSortedByAscendingDate()
+        }
     }
 
     override fun sendMessage(message: Message): String? {
         throw UnsupportedOperationException()
     }
+
+    /*
+     * Local storage
+     */
+
+    override fun storeItems(items: List<RealmModel>) {
+        if (items.isEmpty()) return
+        doAsync {
+            Log.d(TAG, "Storing conversation and message items")
+            items.forEach { storeItem(it) }
+        }
+    }
+
+    override fun storeItem(item: RealmModel) {
+        when (item) {
+            is Conversation -> {
+                Log.d(TAG, "Storing conversation: ${item.id} with partner ${item.partner}")
+                storeItemInRealm(item)
+            }
+            is Message -> {
+                Log.d(TAG, "Storing message: ${item.id} with text: ${item.text}")
+                storeItemInRealm(item)
+            }
+            else -> Log.e(TAG, "Cannot store unknown item!")
+        }
+    }
+
+    private fun storeItemInRealm(item: RealmModel) {
+        executeRealmTransaction {
+            it.copyToRealmOrUpdate(item)
+        }
+    }
+
+    /*
+     * Utilities
+     */
+
+    private fun localUserExists(): Boolean {
+        return userRepository.localUserExists()
+    }
+
+    private fun getLocalUserId(): String {
+        return userRepository.getLocalUserId()
+    }
+
+    /*
+     * Extensions
+     */
+
+    private fun Realm.whereConversation(): RealmQuery<Conversation> {
+        return this.where(Conversation::class.java)
+    }
+
+    private fun Realm.whereMessage(): RealmQuery<Message> {
+        return this.where(Message::class.java)
+    }
+
+    fun <T : RealmModel> RealmQuery<T>.userIdEqualTo(id: String): RealmQuery<T> {
+        return this.equalTo("userId", id)
+    }
+
+    fun <T : RealmModel> RealmQuery<T>.partnerIdEqualTo(id: String): RealmQuery<T> {
+        return this.equalTo("partner.username", id)
+    }
+
+    fun RealmQuery<Message>.conversationIdEqualTo(id: String): RealmQuery<Message> {
+        return this.equalTo("conversationId", id)
+    }
+
+    fun Realm.queryConversation(query: String): RealmQuery<Conversation> {
+        return this.whereConversation()
+                .userIdEqualTo(getLocalUserId())
+                .contains("partner.username", query, Case.INSENSITIVE)
+                .or()
+                .contains("latestMessage.text", query, Case.INSENSITIVE)
+    }
+
 }
